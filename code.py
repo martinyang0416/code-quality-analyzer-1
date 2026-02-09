52be452c066a8cbb22cf36e3ea4bb1d84bf49534
Analyzer <analyzer@test.com>
2026-02-09 13:27:13 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,18 @@
-s = input().strip()
-k = int(input())
+n = int(input())
+weights = list(map(int, input().split()))
+x = weights.count(100)
+y = weights.count(200)
+total = x + 2 * y
 
-result = []
-for char in s:
-    if char.islower():
-        pos = ord(char) - ord('a')
-        if pos < k:
-            result.append(char.upper())
-        else:
-            result.append(char)
-    else:
-        result.append(char)
-
-print(''.join(result))
+if total % 2 != 0:
+    print("NO")
+else:
+    t = total // 2
+    max_a = min(y, t // 2)
+    possible = False
+    for a in range(max_a + 1):
+        required_b = t - 2 * a
+        if 0 <= required_b <= x:
+            possible = True
+            break
+    print("YES" if possible else "NO")
