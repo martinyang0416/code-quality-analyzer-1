cabc068fdf19f2f270bd2f25f6b23e9f92aa7502
Analyzer <analyzer@test.com>
2026-02-09 11:23:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
-f0 = "What are you doing at the end of the world? Are you busy? Will you save us?"
-s1_str = "What are you doing while sending "  # Length 33
-s2_str = "? Are you busy? Will you send "    # Length 30
-s3_char = '?'
+import bisect
 
-def get_char(n, k):
-    if n == 0:
-        if k > len(f0):
-            return '.'
-        else:
-            return f0[k-1]
-    # Check if L_n <k
-    if n < 60:
-        L_n = 135 * (2 ** n) - 64
-        if k > L_n:
-            return '.'
-    current_level = n
-    current_k = k
-    while 
+def putaway(A, B, T, X, Y, W, S):
+    X_sorted = sorted(X)
+    Y_sorted = sorted(Y)
+    
+    Cw = 0  # Only weak
+    Cs = 0  # Only small
+    Cb = 0  # Both
+    
+    for i in range(T):
+        w = W[i]
+        s = S[i]
+        
+        can_weak = bisect.bisect_right(X_sorted, w) < len(X_sorted)
+        can_small = bisect.bisect_right(Y_sorted, s) < len(Y_sorted)
+        
+        if not can_weak and not can_small:
+            return -1
+        
+        if can_weak and not can_small
