b01658f8c66f00b455adb28f0a2bd8cd2c4ab9c0
Analyzer <analyzer@test.com>
2026-02-09 13:33:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,9 @@
-a, b, x1, y1, x2, y2 = map(int, input().split())
+import math
 
-s1 = x1 + y1
-s2 = x2 + y2
-k1 = s1 // (2 * a)
-k2 = s2 // (2 * a)
-lines_a = abs(k2 - k1)
-
-d1 = x1 - y1
-d2 = x2 - y2
-m1 = d1 // (2 * b)
-m2 = d2 // (2 * b)
-lines_b = abs(m2 - m1)
-
-print(max(lines_a, lines_b))
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    m = 2 * n
+    angle = math.pi / (2 * m)
+    result = 1.0 / (2 * math.sin(angle))
+    print("{0:.9f}".format(result))
