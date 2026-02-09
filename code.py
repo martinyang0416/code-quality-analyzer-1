d1ff82cea4c695a822545284fc3ffb40d5afc8bf
Analyzer <analyzer@test.com>
2026-02-09 14:07:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,26 @@
-n = int(input())
+MOD = 998244353
+
+n, m = map(int, input().split())
 a = list(map(int, input().split()))
-b = list(map(int, input().split()))
+w = list(map(int, input().split()))
 
-prefix_a_i = [0] * n
-prefix_b = [0] * n
-prefix_bi_i = [0] * n
-
-prefix_a_i[0] = a[0] * 0
-prefix_b[0] = b[0]
-prefix_bi_i[0] = 0 * b[0]
-
-for i in range(1, n):
-    prefix_a_i[i] = prefix_a_i[i-1] + a[i] * i
-    prefix_b[i] = prefix_b[i-1] + b[i]
-    prefix_bi_i[i] = prefix_bi_i[i-1] + i * b[i]
-
-term_alt = []
+L0 = 0
+D0 = 0
 for i in range(n):
-    term = b[i] * 2 * i + a[i] * (2 * i + 1)
-    term_alt.append(term)
+    if a[i] == 1:
+        L0 = (L0 + w[i]) % MOD
+    else:
+        D0 = (D0 + w[i]) % MOD
 
-suffix_alt = 
+product_liked = 1
+product_disliked = 1
+L = L0
+D = D0
+
+for _ in range(m):
+    S = (L + D) % MOD
+    inv_S = pow(S, MOD-2, MOD)
+    term_liked = (S + 1) * inv_S % MOD
+    term_disliked = (S - 1) * inv_S % MOD
+    
+    product_liked = product_liked * term_
