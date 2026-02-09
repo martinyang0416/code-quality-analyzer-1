a57beeccc3ff1736da902cf8982eb257b2418780
Analyzer <analyzer@test.com>
2026-02-09 14:23:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,13 @@
-def findMaxForm(strs, m, n):
-    dp = [[0] * (n + 1) for _ in range(m + 1)]
-    for s in strs:
-        zeros = s.count('0')
-        ones = len(s) - zeros
-        if zeros > m or ones > n:
-            continue
-        for i in range(m, zeros - 1, -1):
-            for j in range(n, ones - 1, -1):
-                dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
-    return dp[m][n]
+def containsNearbyAlmostDuplicate(nums, k, t):
+    if k < 0 or t < 0:
+        return False
+    bucket_map = {}
+    index_map = {}
+    for i in range(len(nums)):
+        # Remove the element out of window
+        remove_index = i - k - 1
+        if remove_index >= 0:
+            if remove_index in index_map:
+                old_bucket = index_map[remove_index]
+                if old_bucket in bucket_map and bucket_map[old_bucket][1] == remove_index:
+                    del bucket_map[old_bucket]
