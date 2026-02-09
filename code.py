ac5b1d551ca5d7fe51d5588d00e51ae82c7a500c
Analyzer <analyzer@test.com>
2026-02-09 11:56:48 -0800

    Update

--- code.py
+++ code.py
@@ -1,30 +1,24 @@
-import sys
-from collections import deque
-
 def rotate_right(s, N):
-    last = s & 1
-    s >>= 1
-    s |= last << (N - 1)
-    return s
+    return (s >> 1) | ((s & 1) << (N - 1))
 
 def main():
+    import sys
+    from collections import deque
+
     input = sys.stdin.read().split()
     idx = 0
     T = int(input[idx])
     idx += 1
     N = int(input[idx])
     idx += 1
-
+    cases = []
     for _ in range(T):
         L_str = input[idx]
-        S_str = input[idx+1]
-        idx +=2
+        S_str = input[idx + 1]
+        cases.append((L_str, S_str))
+        idx += 2
 
-        L_mask = int(L_str, 2)
-        S_mask = int(S_str, 2)
-        target = L_mask
-
-        visited = {}
-        q = deque()
-
-     
+    for L_str, S_str in cases:
+        L = int(L_str, 2)
+        S = int(S_str, 2)
+        visit
