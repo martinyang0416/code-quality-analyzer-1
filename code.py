e6950187b8238da4e22e49e7bbad4f58ce882be0
Analyzer <analyzer@test.com>
2026-02-09 10:15:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,19 @@
-n = int(input())
-employees = []
-sumT = 0
-for _ in range(n):
-    t, r = map(int, input().split())
-    employees.append((r, t))
-    sumT += t
+n, m, k = map(int, input().split())
+moves = [tuple(map(int, input().split())) for _ in range(k)]
 
-employees.sort(reverse=True, key=lambda x: x[0])
+added = set()
+result = 0
 
-current_sum = 0
-max_val = 0
-for r, t in employees:
-    current_sum += t
-    temp = current_sum + r
-    if temp > max_val:
-        max_val = temp
-
-answer = max(max_val, sumT)
-print(answer)
+for step in range(k):
+    i, j = moves[step]
+    added.add((i, j))
+    
+    # Check all four possible squares that could include (i,j)
+    
+    # Square 1: (i,j) is top-left
+    if i + 1 <= n and j + 1 <= m:
+        if (i, j + 1) in added and (i + 1, j) in added and (i + 1, j + 1) in added:
+            result = step + 1
+            break
+    
+    # Square 2: (i,j) is top-ri
