8336f8dd2078f5a9de0644b1bfa1c72407ec4d59
Analyzer <analyzer@test.com>
2026-02-09 14:51:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,15 @@
-import math
+from collections import defaultdict
 
 class Solution:
-    def bulbSwitch(self, n: int) -> int:
-        return math.isqrt(n)
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        n = len(img1)
+        ones1 = [(i, j) for i in range(n) for j in range(n) if img1[i][j] == 1]
+        ones2 = [(i, j) for i in range(n) for j in range(n) if img2[i][j] == 1]
+        
+        counter = defaultdict(int)
+        max_overlap = 0
+        
+        for i1, j1 in ones1:
+            for i2, j2 in ones2:
+                dx = i2 - i1
+              
