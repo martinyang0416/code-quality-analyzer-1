0d78a4cdecabd0a49ca900ec4d8c47964ec5347f
Analyzer <analyzer@test.com>
2026-02-09 10:49:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,9 @@
 a = int(input())
-binary = format(a, '06b')
-output = ['0'] * 6
+p = [4, 1, 3, 2, 0, 5]  # permutation mapping for each bit position 0-5
+result = 0
 
 for i in range(6):
-    bit = binary[i]
-    p = 6 - i  # original position (1-based)
-    if p == 1:
-        q = 5
-    elif p == 2:
-        q = 2
-    elif p == 3:
-        q = 4
-    elif p == 4:
-        q = 3
-    elif p == 5:
-        q = 1
-    else:  # p == 6
-        q = 6
-    new_index = 6 - q  # because output_str index for position q is (6 - q)
-    output[new_index] = bit
+    if a & (1 << i):
+        result |= 1 << p[i]
 
-result = int(''.join(output), 2)
 print(result)
