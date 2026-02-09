687c71fc29d38291080dd75909a8dd682ae542d0
Analyzer <analyzer@test.com>
2026-02-09 14:28:12 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,19 @@
-import collections
-
-def longestPrefix(nums):
-    current_freq = collections.defaultdict(int)
-    freq_freq = collections.defaultdict(int)
-    max_length = 0
-
-    for i, num in enumerate(nums):
-        old_count = current_freq[num]
-        if old_count > 0:
-            freq_freq[old_count] -= 1
-            if freq_freq[old_count] == 0:
-                del freq_freq[old_count]
-        current_freq[num] += 1
-        new_count = current_freq[num]
-        freq_freq[new_count] += 1
-
-        if len(fre
+def min_sum_of_lengths(arr, target):
+    n = len(arr)
+    if n < 2:
+        return -1
+    
+    # Compute left array
+    prefix_sum = 0
+    prefix_map = {0: -1}
+    left = [float('inf')] * n
+    for i in range(n):
+        prefix_sum += arr[i]
+        needed = prefix_sum - target
+        if needed in prefix_map:
+            j = prefix_map[needed]
+            current_length = i - j
+            if i == 0:
+                left[i] = current_length
+            else:
+                left[i] = min(left[i
