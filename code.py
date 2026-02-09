c11db4ba811e3271b3c49ad6dc5684708292ac9e
Analyzer <analyzer@test.com>
2026-02-09 14:36:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,19 @@
-from typing import List
+MOD = 10**9 + 7
 
-class Solution:
-    def shortestSubarray(self, A: List[int], K: int) -> int:
-        n = len(A)
-        prefix = [0] * (n + 1)
-        for i in range(n):
-            prefix[i+1] = prefix[i] + A[i]
+def numPermsDISequence(S: str) -> int:
+    n = len(S)
+    dp = [[0] * (n + 2) for _ in range(n + 2)]
+    dp[0][0] = 1
+    
+    for i in range(1, n + 1):
+        prefix = [0] * (i + 2)
+        for j in range(i + 1):
+            prefix[j + 1] = (prefix[j] + dp[i - 1][j]) % MOD
         
-        deque = []
-        deque.append(0)
-        min_len = float('inf')
-        
-        for j in range(1, len(prefix)):
-            # Maintain deque to keep prefix in increasing order
-            while deque and prefix[j] <= prefix[deque[-1]]:
-                deque.
+        for j in range(i + 1):
+            if S[i - 1] == 'D':
+                dp[i][j] = (prefix[i] - prefix[j]) % MOD
+            else:
+                dp[i][j] = prefix[j] % MOD
+    
+    return s
