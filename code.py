4bc18230d8612e6ed4e93cc17d8aade7e90d723d
Analyzer <analyzer@test.com>
2026-02-09 10:41:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,22 @@
-import bisect
+import sys
 
-n = int(input())
-arr = list(map(int, input().split()))
-rev_arr = arr[::-1]
+MOD = 10**9 + 7
 
-tails = []
-for num in rev_arr:
-    idx = bisect.bisect_left(tails, num)
-    if idx == len(tails):
-        tails.append(num)
-    else:
-        tails[idx] = num
-
-print(len(tails))
+def main():
+    input = sys.stdin.read().split()
+    idx = 0
+    T = int(input[idx])
+    idx += 1
+    for _ in range(T):
+        n = int(input[idx])
+        m = int(input[idx+1])
+        idx += 2
+        degrees = [0] * (n + 1)  # 1-based indexing
+        for __ in range(m):
+            u = int(input[idx])
+            v = int(input[idx+1])
+            idx += 2
+            degrees[u] += 1
+            degrees[v] += 1
+        
+        # Compute Q = product of (1 + degre
