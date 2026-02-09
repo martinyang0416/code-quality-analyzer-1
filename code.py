69501f4fa63af6631c4deab35d6d47655df9ff71
Analyzer <analyzer@test.com>
2026-02-09 14:11:57 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,2 @@
-n, V = map(int, input().split())
-a = list(map(int, input().split()))
-b = list(map(int, input().split()))
-sum_a = sum(a)
-min_ratio = min(b[i] / a[i] for i in range(n))
-v_ratio = V / sum_a
-x = min(min_ratio, v_ratio)
-total = x * sum_a
-print("{0:.10f}".format(total))
+n = int(input())
+print((n + 1) // 2)
