fbbfc0e3b7094748f52e8c9afc397b7f9a7191e5
Analyzer <analyzer@test.com>
2026-02-09 10:10:25 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,21 @@
-n = int(input())
-b = list(map(int, input().split()))
-sum_all = sum(b)
-active = [True] * (n + 1)  # active[i] indicates if token i is still active
-total_gain = 0
+import sys
+from collections import deque
 
-for x in range(n, 0, -1):
-    current_sum = 0
-    m = x
-    while m <= n:
-        if active[m]:
-            current_sum += -b[m-1]  # b is 0-based
-        m += x
-    if current_sum > 0:
-        total_gain += current_sum
-        m = x
-        while m <= n:
-            if active[m]:
-                active[m] = False
-            m += x
-
-pr
+def main():
+    input = sys.stdin.read
+    data = input().split()
+    ptr = 0
+    t = int(data[ptr])
+    ptr += 1
+    for _ in range(t):
+        n = int(data[ptr])
+        k = int(data[ptr+1])
+        ptr += 2
+        friends = list(map(int, data[ptr:ptr+k]))
+        ptr += k
+        adj = [[] for _ in range(n + 1)]
+        for __ in range(n - 1):
+            u = int(data[ptr])
+            v = int(data[ptr+1])
+            adj[u].append(v)
+            adj
