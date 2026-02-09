9c75156550dbde49f602fcaaef594fa998fea839
Analyzer <analyzer@test.com>
2026-02-09 14:19:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,11 @@
-import heapq
-
-def minRefuelStops(target, startFuel, stations):
-    heap = []
-    prev_pos = 0
-    current_fuel = startFuel
-    stops = 0
-    for pos, fuel in stations:
-        distance = pos - prev_pos
-        while current_fuel < distance:
-            if not heap:
-                return -1
-            current_fuel += -heapq.heappop(heap)
-            stops += 1
-        current_fuel -= distance
-        prev_pos = pos
-        heapq.heappush(heap, -fuel)
-    remaining = target - prev_pos
-    while 
+def hasAllCodes(s: str, k: int) -> bool:
+    if len(s) < k:
+        return False
+    required = 1 << k  # 2^k
+    seen = set()
+    for i in range(len(s) - k + 1):
+        substr = s[i:i+k]
+        seen.add(substr)
+        if len(seen) == required:
+            return True
+    return len(seen) == required
