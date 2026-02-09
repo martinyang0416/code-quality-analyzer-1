1a30185f3b01192a231a22e608fbac0e8138aaec
Analyzer <analyzer@test.com>
2026-02-09 11:33:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,15 @@
 import sys
 
 def main():
-    sys.setrecursionlimit(1 << 25)
     N = int(sys.stdin.readline())
     a = list(map(int, sys.stdin.readline().split()))
     prefix = [0] * (N + 1)
-    for i in range(1, N + 1):
-        prefix[i] = prefix[i - 1] + a[i - 1]
+    for i in range(1, N+1):
+        prefix[i] = prefix[i-1] + a[i-1]
 
-    for i in range(1, N + 1):
-        # Compute S_in: subarrays including i
-        sin = [prefix[r] - prefix[l - 1] for l in range(1, i + 1) for r in range(i, N + 1)]
-        
-        # Compute S_out: subarrays not including i (split into left and ri
+    for current_i in range(1, N+1):
+        i = current_i
+        # Compute S: all subarrays that include i
+        S = [prefix[e] - prefix[s-1] for s in range(1, i+1) for e in range(i, N+1)]
+        # Compute T_left: all subarrays ending before i
+        T_left = [prefix[e] - prefix[s-1] fo
