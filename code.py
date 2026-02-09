97c37262c553ee7ae148256bf7f707b0d7a48f41
Analyzer <analyzer@test.com>
2026-02-09 14:59:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,18 @@
-def threeSumClosest(nums, target):
-    nums.sort()
-    closest_sum = nums[0] + nums[1] + nums[2]
-    n = len(nums)
-    for i in range(n - 2):
-        left, right = i + 1, n - 1
-        while left < right:
-            current_sum = nums[i] + nums[left] + nums[right]
-            if current_sum == target:
-                return current_sum
-            if abs(current_sum - target) < abs(closest_sum - target):
-                closest_sum = current_sum
-            if current_sum < target:
-            
+from collections import defaultdict
+
+class Solution:
+    def queryString(self, S: str, N: int) -> bool:
+        m = N.bit_length()
+        len_S = len(S)
+        subs = defaultdict(set)
+        
+        for l in range(1, m + 1):
+            if l > len_S:
+                continue
+            for i in range(len_S - l + 1):
+                sub = S[i:i+l]
+                subs[l].add(sub)
+        
+        for l in range(1, m):
+            expected = 1 << (l - 1)
+            max_possible = len_S - l +
