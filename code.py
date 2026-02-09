47cb74a52f22fbb78518f0eda11b10500cab44e8
Analyzer <analyzer@test.com>
2026-02-09 10:39:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,20 @@
-def main():
-    text = input().strip()
-    n = len(text)
-    if n == 0:
-        print(0)
-        return
+s = input().strip()
+palindromes = set()
+n = len(s)
 
-    # Constants for double hashing
-    base1 = 911382629
-    mod1 = 10**18 + 3
-    base2 = 35714285
-    mod2 = 10**18 + 7
+for i in range(n):
+    # Check for odd length palindromes centered at i
+    l, r = i, i
+    while l >= 0 and r < n and s[l] == s[r]:
+        palindromes.add(s[l:r+1])
+        l -= 1
+        r += 1
+    
+    # Check for even length palindromes centered between i and i+1
+    l, r = i, i + 1
+    while l >= 0 and r < n and s[l] == s[r]:
+        palindromes.add(s[l:r+1])
+        l -= 1
+        r += 1
 
-    # Precompute prefix and power arrays for the first hash
-    pre1 = [0] * (n + 1)
-    pow1 = [1] * (n + 1)
-    for i in range(1, n + 1):
-        pow1[i] = (pow1[i-1] * base1) % mod1
-    for i in range(1, n + 1):
-        pre1[i] = (pre1[i-1] * base1 + ord(text[i-1])) % 
+print(len(palindromes))
