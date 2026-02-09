e90af55ed851bf75f09441b8a1ecba2203ea7694
Analyzer <analyzer@test.com>
2026-02-09 12:17:18 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,19 @@
-def Calculo_Brute_Force(x):
-    
-    for a in range(1,x+1):
-            if ((a*a)>x) :
-                print (a,a)
-                return(a,a)
-    
-    print(-1)
-    return(-1)
+import sys
+import time
+import itertools
+from itertools import accumulate, product, permutations, combinations
+import collections
+from collections import Counter, OrderedDict, deque, defaultdict, ChainMap
+from functools import lru_cache
+import math
+from math import sqrt, sin, cos, tan, ceil, fabs, floor, gcd, exp, log, log2
+import fractions
+from typing import List, Tuple
+import numpy as np
+import random
+import heapq
+from heapq import *
+from dataclasses import dataclass
 
-Calculo_Brute_Force(int(input()))
+import builtins
+import re
