763537acede176f29895f7acfb27253f7ebbe91c
Analyzer <analyzer@test.com>
2026-02-09 15:16:24 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,19 @@
-import bisect
-from collections import defaultdict
+import sys
+from collections import deque
 
-# Read input
-data = list(map(int, input().split()))
-n = data[0]
-T = data[1]
-S = data[2:2+n]
-
-# Precompute sum_map for all (k, l) pairs where k < l
-sum_map = defaultdict(list)
-for k in range(n):
-    for l in range(k + 1, n):
-        current_sum = S[k] + S[l]
-        sum_map[current_sum].append(k)
-
-# Sort each list in sum_map for binary search
-for s in sum_map:
-    sum_map[s].sort()
-
-total = 0
-
-# Iterate over all (i, j) pairs with i < j
-for j in r
+def main():
+    sys.setrecursionlimit(1 << 25)
+    N = int(sys.stdin.readline())
+    traffic = [0] * (N + 1)
+    for i in range(1, N + 1):
+        traffic[i] = int(sys.stdin.readline())
+    adj = [[] for _ in range(N + 1)]
+    for _ in range(N - 1):
+        u, v = map(int, sys.stdin.readline().split())
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    # Build parent pointers using BFS
+    parent = [0] * (N + 1)
+    visited = [False] * (N + 1)
+  
