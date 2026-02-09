ea5b84ff942564b177e92d111c340bb89cdc0bfd
Analyzer <analyzer@test.com>
2026-02-09 13:28:43 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,22 @@
-class DSU:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [1] * size
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        x_root = self.find(x)
-        y_root = self.find(y)
-        if x_root == y_root:
-            return
-        if self.rank[x_root] < self.rank[y_root]:
-            self.parent[x_root] = y_root
-        else
+ax, ay, bx, by, cx, cy = map(int, input().split())
+
+# Coefficients for perpendicular bisectors of AB and BC
+A1 = 2 * (bx - ax)
+B1 = 2 * (by - ay)
+C1 = (ax**2 + ay**2) - (bx**2 + by**2)
+
+A2 = 2 * (cx - bx)
+B2 = 2 * (cy - by)
+C2 = (bx**2 + by**2) - (cx**2 + cy**2)
+
+# Calculate determinant for line intersection
+D = A1 * B2 - A2 * B1
+
+if D != 0:
+    # Calculate intersection point O
+    x = (B1 * C2 - B2 * C1) / D
+    y = (A2 * C1 - A1 * C2) / D
+
+    # Vectors from O to A, B, C
+    oax = ax - x
+    o
