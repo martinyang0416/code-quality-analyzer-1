99ceec7fb297e555dbfa09e2c66ab45821248183
Analyzer <analyzer@test.com>
2026-02-09 15:00:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,15 @@
-def findSubstringInWraproundString(p):
-    n = len(p)
-    if n == 0:
+def minIncrementForUnique(A):
+    A.sort()
+    moves = 0
+    if len(A) < 2:
         return 0
-    max_run = [1] * n
-    for j in range(n-2, -1, -1):
-        if (ord(p[j+1]) - ord(p[j])) % 26 == 1:
-            max_run[j] = max_run[j+1] + 1
+    prev = A[0]
+    for i in range(1, len(A)):
+        current = A[i]
+        if current <= prev:
+            required = prev + 1
+            moves += required - current
+            prev = required
         else:
-            max_run[j] = 1
-    max_char = [0] * 26
-    for j in range(n):
-        c = p[j]
-        idx = ord(c) - ord('a')
-        if max_run[j] > max_char[idx]:
-            max_char[idx] = max_run[j]
-    return sum(max_char)
+            prev = current
+    return moves
