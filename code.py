1cae0bfeed3ca2f9dc405765277d53330877fa25
Analyzer <analyzer@test.com>
2026-02-09 15:04:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,16 @@
-MOD = 10**9 + 7
-max_fact = 5000
-
-# Precompute factorial and inverse factorial modulo MOD
-fact = [1] * (max_fact + 1)
-for i in range(1, max_fact + 1):
-    fact[i] = fact[i-1] * i % MOD
-
-inv_fact = [1] * (max_fact + 1)
-inv_fact[max_fact] = pow(fact[max_fact], MOD-2, MOD)
-for i in range(max_fact - 1, -1, -1):
-    inv_fact[i] = inv_fact[i+1] * (i+1) % MOD
-
-def comb(m, r):
-    if r < 0 or r > m:
-        return 0
-    return fact[m] * inv_fact[r] % MOD * inv_fact[m - r] % MOD
+import math
 
 T = int(input())
-for _ in
+for _ in range(T):
+    N, L = map(int, input().split())
+    m = 0
+    while True:
+        m += 1
+        total = 0
+        for i in range(1, N + 1):
+            if i > m:
+                continue
+            total += math.comb(m, i)
+        if total >= L:
+            print(m)
+            break
