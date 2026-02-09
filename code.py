f7cea8c85ce4cde4f8b07a633fbdca870b372270
Analyzer <analyzer@test.com>
2026-02-09 13:23:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,21 @@
-n, m = map(int, input().split())
-students = [tuple(map(int, input().split())) for _ in range(n)]
-checkpoints = [tuple(map(int, input().split())) for _ in range(m)]
+def subtract_one(s):
+    s_list = list(s)
+    i = len(s_list) - 1
+    while i >= 0 and s_list[i] == '0':
+        s_list[i] = '9'
+        i -= 1
+    if i < 0:
+        return '0'
+    s_list[i] = str(int(s_list[i]) - 1)
+    new_s = ''.join(s_list).lstrip('0')
+    return new_s if new_s else '0'
 
-for a, b in students:
-    min_dist = float('inf')
-    best_j = 0
-    for idx in range(m):
-        c, d = checkpoints[idx]
-        dist = abs(a - c) + abs(b - d)
-        if dist < min_dist:
-            min_dist = dist
-            best_j = idx + 1
-    print(best_j)
+def compute(s, m):
+    if m == 0:
+        return 0
+    X = list(map(int, s))
+    n = len(X)
+    from functools import lru_cache
+
+    @lru_cache(maxsize=None)
+    def dp(pos, mod, prev_digit, last_dir, tight,
