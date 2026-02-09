6d50f848caa09a05405c3c410b2ac26d21abc811
Analyzer <analyzer@test.com>
2026-02-09 14:09:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,5 @@
-q, x = map(int, input().split())
-count = [0] * x
-current_mex = 0
-
-for _ in range(q):
-    y = int(input())
-    r = y % x
-    count[r] += 1
-    while current_mex // x < count[current_mex % x]:
-        current_mex += 1
-    print(current_mex)
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    chairs = [2 * i for i in range(n, 2 * n)]
+    print(' '.join(map(str, chairs)))
