0d7c15d2dd6680cd693dd2ba8e4c1968b12366a2
Analyzer <analyzer@test.com>
2026-02-09 13:04:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,24 @@
 n = int(input())
-s = input().strip()
-count_i = s.count('I')
-result = 0
-for c in s:
-    if c == 'F':
-        continue
-    if (c == 'A' and count_i == 0) or (c == 'I' and count_i == 1):
-        result += 1
-print(result)
+points = set()
+x_to_ys = {}
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    points.add((x, y))
+    if x not in x_to_ys:
+        x_to_ys[x] = []
+    x_to_ys[x].append(y)
+
+count = 0
+
+for x in x_to_ys:
+    ys = sorted(x_to_ys[x])
+    m = len(ys)
+    for i in range(m):
+        for j in range(i + 1, m):
+            d = ys[j] - ys[i]
+            x_new = x + d
+            if (x_new, ys[i]) in points and (x_new, ys[j]) in points:
+                count += 1
+
+print(count)
