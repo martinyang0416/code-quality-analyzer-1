15df36a70a68de2c4b87679802adfe33b9cdd7c9
Analyzer <analyzer@test.com>
2026-02-09 10:29:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,2 @@
-def main():
-    import sys
-
-    # Read the first line: x, y, z
-    x, y, z = map(int, sys.stdin.readline().split())
-    
-    # Read the number of monitors
-    n = int(sys.stdin.readline())
-    
-    # Read each monitor's cost and type
-    hdmi = []
-    vga = []
-    for _ in range(n):
-        parts = sys.stdin.readline().split()
-        cost = int(parts[0])
-        typ = parts[1]
-        if typ == 'HDMI':
-            hdmi.append(cost)
-        else:
-            vga.append(cost)
-    
-    # Sort the 
+s = int(input())
+print(2 * s - 1)
