6f3cda9a1a3b3d17b75e373c86a295fad145e43c
Analyzer <analyzer@test.com>
2026-02-09 13:06:03 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,18 @@
-class FenwickTree:
-    def __init__(self, size):
-        self.n = size
-        self.tree = [0] * (self.n + 2)  # 1-based indexing
+# Read initial potential victims
+current = input().split()
+pairs = [current]
 
-    def update(self, idx, delta):
-        while idx <= self.n:
-            self.tree[idx] += delta
-            idx += idx & -idx
+n = int(input())
+for _ in range(n):
+    murdered, replacement = input().split()
+    last = pairs[-1]
+    # Determine the new pair after replacement
+    if last[0] == murdered:
+        new_pair = [replacement, last[1]]
+    else:
+        new_pair = [last[0], replacement]
+    pairs.append(new_pair)
 
-    def query(self, idx):
-        res = 0
-        while idx > 0:
-            res += self.tree[idx]
-            idx -= idx & -idx
-        return res
-
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    ptr = 0
-    n = int(in
+# Output each pair
+for pair in pairs:
+    print(' '.join(pair))
