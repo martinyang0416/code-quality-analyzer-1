bfed5e47175299aaec90e12857af4bc0abfe298c
Analyzer <analyzer@test.com>
2026-02-09 11:34:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,26 @@
-import sys
 import bisect
 
 def main():
-    N = int(sys.stdin.readline())
-    a = list(map(int, sys.stdin.readline().split()))
-
-    # Compute prefix sums
+    import sys
+    input = sys.stdin.read().split()
+    idx = 0
+    N = int(input[idx])
+    idx += 1
+    a = list(map(int, input[idx:idx+N]))
+    idx += N
+    S = sorted(a)
     prefix = [0] * (N + 1)
-    for i in range(1, N + 1):
-        prefix[i] = prefix[i - 1] + a[i - 1]
-
     for i in range(N):
-        # Current index is i (0-based)
-        L_list = prefix[0:i+1]
-        R_list = prefix[i+1:N+1]
-
-        # Compute S_group: all R-L for R in R_list, L in L_list
-        S_group = [R - L for L in L_list for R in R_li
+        prefix[i+1] = prefix[i] + S[i]
+    
+    T = 0
+    for i in range(N):
+        T += S[i] * (i + 1)
+    
+    Q = int(input[idx])
+    idx += 1
+    for _ in range(Q):
+        i = int(input[idx])
+        j = int(input[idx+1])
+        idx += 2
+     
