ab34f4ab0a2a9dfa4b6ba1c5e04fde1dd0ba25f4
Analyzer <analyzer@test.com>
2026-02-09 15:03:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,25 @@
-n, *rest = map(int, open(0).read().split())
-a = rest[:n]
-total_xor = 0
-for num in a:
-    total_xor ^= num
-result = [x ^ total_xor for x in a]
-print(' '.join(map(str, result)))
+N, M = map(int, input().split())
+
+def factorize(n):
+    factors = {}
+    while n % 2 == 0:
+        factors[2] = factors.get(2, 0) + 1
+        n = n // 2
+    i = 3
+    while i * i <= n:
+        while n % i == 0:
+            factors[i] = factors.get(i, 0) + 1
+            n = n // i
+        i += 2
+    if n > 1:
+        factors[n] = 1
+    return factors
+
+factors = factorize(N)
+
+divisors = [(1, 1)]
+for p, e in factors.items():
+    temp = []
+    for d, mu in divisors:
+        for k in range(e + 1):
+  
