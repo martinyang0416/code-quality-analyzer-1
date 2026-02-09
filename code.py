25919cddbc2ef8f694661849a4eb74f80761117c
Analyzer <analyzer@test.com>
2026-02-09 13:58:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,7 +1,15 @@
-t = int(input())
-for _ in range(t):
-    c, d = map(int, input().split())
-    if c == d:
-        print(-1)
-    else:
-        print(c * d - c - d)
+n, L, a = map(int, input().split())
+if n == 0:
+    print(L // a)
+else:
+    breaks = 0
+    prev_end = 0
+    for _ in range(n):
+        t, l = map(int, input().split())
+        gap = t - prev_end
+        breaks += gap // a
+        prev_end = t + l
+    # Check the time after the last customer
+    gap = L - prev_end
+    breaks += gap // a
+    print(breaks)
