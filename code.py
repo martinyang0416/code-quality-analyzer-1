6e197288cbc6afa533181bf4b01a0d94515542d3
Analyzer <analyzer@test.com>
2026-02-09 13:40:28 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,24 @@
-s = input().strip()
-if len(s) < 3:
-    print(0)
-else:
-    mid = len(s) // 2
-    print(s[mid-1:mid+2])
+import sys
+
+MOD = 10**9 + 7
+
+class FenwickTree:
+    def __init__(self, size):
+        self.n = size
+        self.tree = [0] * (self.n + 1)
+    
+    def update(self, idx, delta):
+        while idx <= self.n:
+            self.tree[idx] += delta
+            idx += idx & -idx
+    
+    def query(self, idx):
+        res = 0
+        while idx > 0:
+            res += self.tree[idx]
+            idx -= idx & -idx
+        return res
+
+def main():
+    n, q = map(int, sys.stdin.readline().split())
+    a = lis
