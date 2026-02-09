67da17ef746183b6ac91c2804cb88ab1c94422ee
Analyzer <analyzer@test.com>
2026-02-09 14:46:51 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,22 @@
-def hIndex(citations):
-    citations.sort()
-    n = len(citations)
-    max_h = 0
-    for i in range(n):
-        current_h = min(citations[i], n - i)
-        if current_h > max_h:
-            max_h = current_h
-    return max_h
+import math
+
+def nthUglyNumber(n: int, a: int, b: int, c: int) -> int:
+    def lcm(x, y):
+        return x * y // math.gcd(x, y)
+    
+    ab = lcm(a, b)
+    ac = lcm(a, c)
+    bc = lcm(b, c)
+    abc = lcm(ab, c)
+    
+    left, right = 1, 2 * 10**9
+    
+    def count(x):
+        return x // a + x // b + x // c - x // ab - x // ac - x // bc + x // abc
+    
+    while left < right:
+        mid = (left + right) // 2
+        if count(mid) < n:
+            left = mid + 1
+        else:
+            right
