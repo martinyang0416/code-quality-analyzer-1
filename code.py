36e8ef3eda3ba04c612bcfa8dfeb6271727c3f78
Analyzer <analyzer@test.com>
2026-02-09 14:17:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,12 @@
-def shortestCommonSupersequence(str1: str, str2: str) -> str:
-    m, n = len(str1), len(str2)
+def longestCommonSubsequence(text1: str, text2: str) -> int:
+    m, n = len(text1), len(text2)
     dp = [[0] * (n + 1) for _ in range(m + 1)]
     
     for i in range(1, m + 1):
         for j in range(1, n + 1):
-            if str1[i-1] == str2[j-1]:
+            if text1[i-1] == text2[j-1]:
                 dp[i][j] = dp[i-1][j-1] + 1
             else:
                 dp[i][j] = max(dp[i-1][j], dp[i][j-1])
     
-    i, j = m, n
-    result = []
-    while i > 0 and j > 0:
-        if str1[i-1] == str2[j-1]:
-            result.append(str1[i-1]
+    return dp[m][n]
