15145d38ca01f8e80f4926af544f01f31dcfdd6b
Analyzer <analyzer@test.com>
2026-02-09 11:15:30 -0800

    Update

--- code.py
+++ code.py
@@ -1,3 +1,12 @@
-a, b = map(int, input().split())
-rev_b = int(str(b)[::-1])
-print(a + rev_b)
+a1, a2, a3 = map(int, input().split())
+terms_needed = a3 + 1
+
+if terms_needed == 1:
+    print(a1)
+elif terms_needed == 2:
+    print(a2)
+else:
+    for _ in range(terms_needed - 2):
+        next_val = a1 + a2
+        a1, a2 = a2, next_val
+    print(a2)
