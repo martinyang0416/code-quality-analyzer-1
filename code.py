19b6442bacdc4df5c1a569a4057e761e697ac93f
Analyzer <analyzer@test.com>
2026-02-09 14:34:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,9 @@
-def minCost(n, cuts):
-    cuts_sorted = sorted(cuts)
-    sorted_cuts = [0] + cuts_sorted + [n]
-    m = len(sorted_cuts)
-    dp = [[0] * m for _ in range(m)]
-    
-    for l in range(2, m):
-        for i in range(m - l):
-            j = i + l
-            dp[i][j] = float('inf')
-            for k in range(i + 1, j):
-                if dp[i][k] + dp[k][j] < dp[i][j]:
-                    dp[i][j] = dp[i][k] + dp[k][j]
-            dp[i][j] += sorted_cuts[j] - sorted_cuts[i]
-    
-    return dp[0][m-1]
+def isSelfCrossing(x):
+    n = len(x)
+    for i in range(n):
+        if i >= 3 and x[i] >= x[i-2] and x[i-1] <= x[i-3]:
+            return True
+        if i >= 5 and x[i-2] >= x[i-4] and x[i] >= x[i-2] - x[i-4] \
+           and x[i-1] >= x[i-3] - x[i-5] and x[i-3] >= x[i-5]:
+            return True
+    return False
