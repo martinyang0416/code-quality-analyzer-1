02c1e56e9b0249873fc7815ddb76ac961a09ffcf
Analyzer <analyzer@test.com>
2026-02-09 13:56:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,23 @@
 import sys
-from collections import defaultdict
+from collections import deque
 
-n, a = map(int, sys.stdin.readline().split())
-x = list(map(int, sys.stdin.readline().split()))
+def precompute_tetra(max_num):
+    tetra = []
+    n = 1
+    while True:
+        t = n * (n + 1) * (n + 2) // 6
+        if t > max_num:
+            break
+        tetra.append(t)
+        n += 1
+    tetra.sort(reverse=True)  # Sort in descending order for efficiency
+    return tetra
 
-dp = [defaultdict(int) for _ in range(n+1)]
-dp[0][0] = 1
-
-for num in x:
-    for k in reversed(range(n)):
-        for s, cnt in list(dp[k].items()):
-            new_s = s + num
-            dp[k+1][new_s] += cnt
-
-result = 0
-for k in range(1, n+1):
-    target = a * k
-    result += dp[k].get(target, 0)
-
-print(result)
+def precompute_odd_tetra(max_num):
+    tetra = []
+    n = 1
+    while True:
+        t = n * (n + 1) * (n + 2) // 6
+        if t > max_num:
+            break
+        if n % 4 ==
