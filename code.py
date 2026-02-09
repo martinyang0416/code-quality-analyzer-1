f15a43eedb24759c7946f93c87f26cacde07f92f
Analyzer <analyzer@test.com>
2026-02-09 10:18:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,19 @@
-n = int(input())
-MOD = 10**9 + 7
+def main():
+    import sys
+    M = int(sys.stdin.readline())
+    B = list(map(int, sys.stdin.readline().split()))
+    if M < 4:
+        print(0)
+        return
 
-max_fact = 2 * n
-fact = [1] * (max_fact + 1)
-for i in range(1, max_fact + 1):
-    fact[i] = fact[i-1] * i % MOD
-
-term = fact[2 * n]
-term = term * pow(fact[n], MOD - 2, MOD) % MOD
-term = term * pow(fact[n], MOD - 2, MOD) % MOD
-
-term = term * pow(n + 1, MOD - 2, MOD) % MOD
-
-print(term)
+    INF = float('inf')
+    prev_dp = [INF] * 4  # [gap0, gap1, gap2, gap3]
+    # Initialize first element (index 0)
+    prev_dp[0] = B[0]       # selected
+    prev_dp[1] = 0          # not selected
+    
+    for i in range(1, M):
+        current_dp = [INF] * 4
+        current_val = B[i]
+        for gap_prev in range(4):
+            if pre
