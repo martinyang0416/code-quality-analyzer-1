5ad9794023cb7ae98285a73def6b270e16bff9c5
Analyzer <analyzer@test.com>
2026-02-09 15:12:46 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,20 @@
-n, x = map(int, input().split())
-A = list(map(int, input().split()))
-original_sum = sum(A)
-delta = [(a / x) - a for a in A]
+import sys
 
-if n == 0:
-    min_sub = 0
-else:
-    current_min = delta[0]
-    min_sub = delta[0]
-    for i in range(1, n):
-        current_min = min(delta[i], current_min + delta[i])
-        if current_min < min_sub:
-            min_sub = current_min
+def main():
+    n = int(sys.stdin.readline())
+    events = []
+    for _ in range(n):
+        s, d = map(int, sys.stdin.readline().split())
+        end = s + d - 1
+        events.append((end, s))
+    events.sort()
+    count = 0
+    last_end = -1
+    for end, s in events:
+        if s > last_end + 1:
+            count += 1
+            last_end = end
+    print(count)
 
-sum_delta = min(min_sub, 0.0)
-result = original_sum + sum_delta
-print("{0:.10f}".format(result))
+if __name__ == "__main__":
+    main()
