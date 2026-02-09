5ba5a95a55d69437971312d351a33f3d4025ccd9
Analyzer <analyzer@test.com>
2026-02-09 10:46:22 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,10 @@
-import sys
-
-def main():
-    n = int(sys.stdin.readline())
-    result = []
-    for _ in range(n):
-        a, b, c, d, e = map(int, sys.stdin.readline().split())
-        # Treat the five numbers as a base4 number
-        num = a * (4**4) + b * (4**3) + c * (4**2) + d * 4 + e
-        index = num % 26
-        result.append(chr(ord('a') + index))
-    print(''.join(result))
-
-if __name__ == "__main__":
-    main()
+n = int(input())
+result = []
+for _ in range(n):
+    nums = list(map(int, input().split()))
+    val = 0
+    for num in nums:
+        val = val * 10 + num
+    idx = val % 26
+    result.append(chr(97 + idx))
+print(''.join(result))
