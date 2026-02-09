4c2b5351dedc509381990a4a94c63d21dbb91d68
Analyzer <analyzer@test.com>
2026-02-09 11:16:57 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,3 @@
-a1, a2, a3 = map(int, input().split())
-term_num = a3 + 1
-a, b = a1, a2
-if term_num == 2:
-    print(b)
-else:
-    for _ in range(3, term_num + 1):
-        next_term = a + b
-        a, b = b, next_term
-    print(b)
+s = input().strip()
+last_digit = s[-1]
+print(0 if int(last_digit) % 2 == 0 else 1)
