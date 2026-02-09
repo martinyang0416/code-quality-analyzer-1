a0b8fd4bb543bc199c50e0dc920dbeaa43010fea
Analyzer <analyzer@test.com>
2026-02-09 13:46:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,25 +1,5 @@
+import math
+
 n, x = map(int, input().split())
-a = list(map(int, input().split()))
-a.sort()
-
-max_happy = 0
-
-for k in range(n, -1, -1):
-    if k > len(a):
-        continue
-    sum_S = sum(a[:k])
-    remaining = a[k:]
-    zeros_remaining = sum(1 for num in remaining if num == 0)
-    
-    if sum_S + zeros_remaining > x:
-        continue
-    delta = x - sum_S - zeros_remaining
-    if delta < 0:
-        continue
-    
-    if zeros_remaining > 0:
-        print(k)
-        exit()
-    else:
-        m = len(remaining)
- 
+g = math.gcd(n, x)
+print(3 * (n - g))
