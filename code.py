bf88ef40525d9219df5176602b12a2cf01ed3c81
Analyzer <analyzer@test.com>
2026-02-09 14:01:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,15 @@
 n = int(input())
-a = list(map(int, input().split()))
-prefix = [0] * (n + 1)
-for i in range(1, n + 1):
-    prefix[i] = prefix[i - 1] + max(0, a[i - 1])
+s = input().strip()
+k = s.count('H')
 
-from collections import defaultdict
-d = defaultdict(list)
-for idx, num in enumerate(a):
-    d[num].append(idx)
-
-max_sum = -float('inf')
-best_i = best_j = -1
-
-for v in d:
-    indices = d[v]
-    if len(indices) < 2:
-        continue
-    first_i = indices[0]
-    last_j = indices[-1]
-    current_sum = (a[first_i] + a[last_j]) + (prefix[last_j] - pref
+if k == 0 or k == n:
+    print(0)
+else:
+    s_double = s + s
+    min_swaps = float('inf')
+    for i in range(n):
+        window = s_double[i:i+k]
+        t_count = window.count('T')
+        if t_count < min_swaps:
+            min_swaps = t_count
+    print(min_swaps)
