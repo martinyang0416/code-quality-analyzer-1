8a13c93ffd54f4d2c2be27a4b07ab5d994477820
Analyzer <analyzer@test.com>
2026-02-09 10:08:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,25 @@
-n, k = map(int, input().split())
+import heapq
 
-count_div3 = n // 3
-
-if k <= count_div3:
-    print(3 * k)
-else:
-    m = k - count_div3
-    # Calculate the m-th non-divisible number
-    g = (m - 1) // 2
-    r = (m - 1) % 2 + 1
-    x = 3 * g + r
-    print(x)
+def main():
+    n = int(input())
+    s = input().strip()
+    e = list(map(int, input().split()))
+    
+    next = [-1] * n
+    prev = [-1] * n
+    for i in range(n):
+        if i > 0:
+            prev[i] = i - 1
+        if i < n - 1:
+            next[i] = i + 1
+    
+    heap = []
+    for i in range(n - 1):
+        j = i + 1
+        if s[i] != s[j]:
+            diff = abs(e[i] - e[j])
+            heapq.heappush(heap, (diff, i, j))
+    
+    output = []
+    while heap:
+        diff, i,
