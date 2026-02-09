0cb554c60904b37b7759378f6fe6b34bdd869aa3
Analyzer <analyzer@test.com>
2026-02-09 13:24:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,7 @@
-from collections import Counter
-
-def flames_result(name1, name2):
-    # Process names and calculate remaining letters count
-    name1 = name1.upper().replace(' ', '')
-    name2 = name2.upper().replace(' ', '')
-    
-    cnt1 = Counter(name1)
-    cnt2 = Counter(name2)
-    
-    common = cnt1 & cnt2
-    cnt1.subtract(common)
-    cnt2.subtract(common)
-    
-    n = sum(cnt1.values()) + sum(cnt2.values())
-    
-    if n == 0:
-        return 'FRIENDS'  # Edge case when all letters are cancelled
-    
-    
+n = int(input())
+a = list(map(int, input().split()))
+sum_elodreip = sum(a)
+max_a = max(a)
+required_k = (2 * sum_elodreip + n) // n
+k = max(required_k, max_a)
+print(k)
