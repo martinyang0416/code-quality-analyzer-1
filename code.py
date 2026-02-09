530653660017054063ff49a734ddc6c5c536c9ce
Analyzer <analyzer@test.com>
2026-02-09 15:11:29 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,9 @@
-import bisect
-import sys
-
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    T = int(input[ptr])
-    ptr += 1
-    for _ in range(T):
-        S = input[ptr]
-        ptr += 1
-        N = len(S)
-        opens = [0] * (N + 1)
-        closes = [0] * (N + 1)
-        open_positions = []
-        for i in range(N):
-            opens[i+1] = opens[i] + (1 if S[i] == '(' else 0)
-            closes[i+1] = closes[i] + (1 if S[i] == ')' else 0)
-            if S[i] == '(':
-                open_pos
+n = int(input())
+girls = list(map(int, input().split()))
+boys = []
+for s in girls:
+    if s == 2:
+        boys.append(s ^ 3)
+    else:
+        boys.append(s ^ 2)
+print(' '.join(map(str, boys)))
