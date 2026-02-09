530c37077053ea56e1f2e15527df73243abf3413
Analyzer <analyzer@test.com>
2026-02-09 13:51:24 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,8 @@
-n = int(input())
-items = []
-for _ in range(n):
-    t, c = map(int, input().split())
-    w = t + 1
-    items.append((w, c))
-
-INF = float('inf')
-dp = [INF] * (n + 1)
-dp[0] = 0
-
-for w, c in items:
-    for s in range(n, -1, -1):
-        if dp[s] != INF:
-            new_s = s + w
-            if new_s > n:
-                new_s = n
-            if dp[new_s] > dp[s] + c:
-                dp[new_s] = dp[s] + c
-
-print(dp[n])
+s = input().strip()
+stack = []
+for c in s:
+    if stack and stack[-1] == c:
+        stack.pop()
+    else:
+        stack.append(c)
+print("Yes" if not stack else "No")
