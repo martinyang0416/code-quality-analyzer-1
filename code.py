73a38de035a7e4f74ec3e7f5086999b76abfa7fe
Analyzer <analyzer@test.com>
2026-02-09 13:05:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,24 +1,22 @@
-n = int(input())
-points = set()
-x_to_ys = {}
+class FenwickTree:
+    def __init__(self, size):
+        self.n = size
+        self.tree = [0] * (self.n + 2)  # 1-based indexing
 
-for _ in range(n):
-    x, y = map(int, input().split())
-    points.add((x, y))
-    if x not in x_to_ys:
-        x_to_ys[x] = []
-    x_to_ys[x].append(y)
+    def update(self, idx, delta):
+        while idx <= self.n:
+            self.tree[idx] += delta
+            idx += idx & -idx
 
-count = 0
+    def query(self, idx):
+        res = 0
+        while idx > 0:
+            res += self.tree[idx]
+            idx -= idx & -idx
+        return res
 
-for x in x_to_ys:
-    ys = sorted(x_to_ys[x])
-    m = len(ys)
-    for i in range(m):
-        for j in range(i + 1, m):
-            d = ys[j] - ys[i]
-            x_new = x + d
-            if (x_new, ys[i]) in points and (x_new, ys[j]) in points:
-                count += 1
-
-print(count)
+def main():
+    import sys
+    input = sys.stdin.read().split()
+    ptr = 0
+    n = int(in
