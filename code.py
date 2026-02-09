9939532fcaac7091cb98230668ed316fdd4d1acf
Analyzer <analyzer@test.com>
2026-02-09 13:32:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,20 @@
-T = int(input())
-for _ in range(T):
-    n = int(input())
-    c4n = (4 * n * (4 * n - 1) * (4 * n - 2)) // 6
-    cn = (n * (n - 1) * (n - 2)) // 6
-    print(c4n - 4 * cn)
+n, m = map(int, input().split())
+
+groups = [[] for _ in range(n + 1)]
+
+for _ in range(m):
+    a_i, b_i = map(int, input().split())
+    delta = (b_i - a_i) % n
+    groups[a_i].append(delta)
+
+max_time = [0] * (n + 1)
+
+for a in range(1, n + 1):
+    deltas = groups[a]
+    if not deltas:
+        continue
+    deltas_sorted = sorted(deltas, reverse=True)
+    current_max = 0
+    for i in range(len(deltas_sorted)):
+        current = i * n + deltas_sorted[i]
+        current_max = max(current_max, current)
