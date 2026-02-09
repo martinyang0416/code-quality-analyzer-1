62d98fd458bcdb2b799a593131a0b1c58454aae6
Analyzer <analyzer@test.com>
2026-02-09 11:04:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,2 @@
-a = int(input())
-sum_digits = sum(int(d) for d in str(a))
-if a % 2 == 1 and sum_digits % 2 == 0:
-    print(1)
-else:
-    print(0)
+n = input().strip()
+print(1 if any(c in {'2','3','5','7'} for c in n) else 0)
