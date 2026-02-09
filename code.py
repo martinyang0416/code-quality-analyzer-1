ace352cbbe84336572ad85051dd0bf65993e6dc6
Analyzer <analyzer@test.com>
2026-02-09 11:24:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,18 @@
 import bisect
 
 def putaway(A, B, T, X, Y, W, S):
-    X_sorted = sorted(X)
-    Y_sorted = sorted(Y)
-    
-    Cw = 0  # Only weak
-    Cs = 0  # Only small
-    Cb = 0  # Both
-    
-    for i in range(T):
-        w = W[i]
-        s = S[i]
-        
-        can_weak = bisect.bisect_right(X_sorted, w) < len(X_sorted)
-        can_small = bisect.bisect_right(Y_sorted, s) < len(Y_sorted)
-        
-        if not can_weak and not can_small:
-            return -1
-        
-        if can_weak and not can_small
+    # Categorize the toys into W_only, S_only, Both
+    W_only = []
+    S_only = []
+    Both = []
+    for w, s in zip(W, S):
+        can_weak = any(w < x for x in X) if A > 0 else False
+        can_small = any(s < y for y in Y) if B > 0 else False
+        if can_weak and can_small:
+            Both.append((w, s))
+        elif can_weak:
+            W_only.append(w)
+        elif can_small:
+            S_only.append(s)
+        else:
+            retur
