48ecd41b588475ed8573091e1a5a95070e5e812a
Analyzer <analyzer@test.com>
2026-02-09 13:49:47 -0800

    Update

--- code.py
+++ code.py
@@ -1,23 +1,20 @@
-MOD = 998244353
-
-def main():
-    import sys
-    from collections import defaultdict
-
-    n, c = map(int, sys.stdin.readline().split())
-    a = list(map(int, sys.stdin.readline().split()))
-    
-    count = defaultdict(int)
-    for num in a:
-        count[num] += 1
-
-    # Check if all colors are present
-    all_present = True
-    for x in range(1, c + 1):
-        if count.get(x, 0) == 0:
-            all_present = False
-            break
-
-    # Compute X for p=0
-    if not all_present:
-        X = 
+t = int(input())
+for _ in range(t):
+    s, c = input().split()
+    if s < c:
+        print(s)
+        continue
+    s_list = list(s)
+    n = len(s_list)
+    found = False
+    for i in range(n):
+        min_char = s_list[i]
+        min_pos = -1
+        for j in range(i + 1, n):
+            if s_list[j] < min_char:
+                min_char = s_list[j]
+                min_pos = j
+            elif s_list[j] == min_char:
+                min_pos = j
+        if min_pos != -1 and min_char < s_list[i]:
+  
