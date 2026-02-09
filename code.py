4538e34a2a9ec212a05fa7948580035379ad16ac
Analyzer <analyzer@test.com>
2026-02-09 14:19:12 -0800

    Update

--- code.py
+++ code.py
@@ -1,12 +1,19 @@
-def maxDiff(num):
-    s = str(num)
-    valid = set()
-    for x in '0123456789':
-        for y in '0123456789':
-            new_s = s.replace(x, y)
-            if new_s[0] == '0' or new_s == '0':
-                continue
-            valid.add(int(new_s))
-    max_val = max(valid)
-    min_val = min(valid)
-    return max_val - min_val
+import heapq
+
+def minRefuelStops(target, startFuel, stations):
+    heap = []
+    prev_pos = 0
+    current_fuel = startFuel
+    stops = 0
+    for pos, fuel in stations:
+        distance = pos - prev_pos
+        while current_fuel < distance:
+            if not heap:
+                return -1
+            current_fuel += -heapq.heappop(heap)
+            stops += 1
+        current_fuel -= distance
+        prev_pos = pos
+        heapq.heappush(heap, -fuel)
+    remaining = target - prev_pos
+    while 
