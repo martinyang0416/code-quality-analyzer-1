81a0b8e09d1b2a5fd28aaf619b81ea0b0e4e1c37
Analyzer <analyzer@test.com>
2026-02-09 14:09:56 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,7 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    chairs = [2 * i for i in range(n, 2 * n)]
-    print(' '.join(map(str, chairs)))
+nums = list(map(int, input().split()))
+a1 = nums[1]
+a2 = nums[2]
+a3 = nums[3] if len(nums) > 3 else 0
+a4 = nums[4] if len(nums) > 4 else 0
+result = (a1 + a2 + a3 + a4) * a3
+print(result)
