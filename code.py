d056841747ca4e0bc71da7e0eacc00f390d99b6c
Analyzer <analyzer@test.com>
2026-02-09 14:26:48 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,14 @@
-def find_poisoned_duration(time_series, duration):
-    if not time_series:
-        return 0
-    total = 0
-    start = time_series[0]
-    end = start + duration - 1
-    for t in time_series[1:]:
-        current_end = t + duration - 1
-        if t > end:
-            total += end - start + 1
-            start = t
-            end = current_end
-        else:
-            end = max(end, current_end)
-    total += end - start + 1
-    return total
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def numSubarraysWithSum(self, A: List[int], S: int) -> int:
+        count = defaultdict(int)
+        count[0] = 1
+        prefix_sum = 0
+        res = 0
+        for num in A:
+            prefix_sum += num
+            res += count.get(prefix_sum - S, 0)
+            count[prefix_sum] += 1
+        return res
