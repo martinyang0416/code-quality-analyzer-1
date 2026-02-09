e87fea4226df50fd359be4d9a117955aad0ca498
Analyzer <analyzer@test.com>
2026-02-09 10:06:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,18 @@
-def calculate_total_segments(x, y):
-    segment_map = {
-        '0': 6, '1': 2, '2': 5, '3': 5, '4': 4,
-        '5': 5, '6': 6, '7': 3, '8': 7, '9': 6,
-        '-': 4
-    }
-    
-    total = 0
-    for number in range(x, y + 1):
-        s = str(number)
-        for c in s:
-            total += segment_map[c]
-        if number != y:
-            total += segment_map['-']
-    
-    return total
+import sys
 
-x, y = map(int, input().split())
-print(calculate_total_segments(x, y))
+def main():
+    m = int(sys.stdin.readline())
+    b = list(map(int, sys.stdin.readline().split()))
+    p = int(sys.stdin.readline())
+    
+    bonuses = dict()  # key: (r, v), value: max c
+    
+    for _ in range(p):
+        rj, cj, vj = map(int, sys.stdin.readline().split())
+        rj -= 1  # zero-based index for resources
+        if vj == 0:
+            if (rj, cj) in bonuses:
+                del bonuses[(rj, cj)]
+        else:
+            key = (rj, vj)
+            if key in bonus
