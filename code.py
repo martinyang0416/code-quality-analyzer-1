3361ea5f287289d292b791afb0a8f53049200f0e
Analyzer <analyzer@test.com>
2026-02-09 15:09:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,12 @@
-def main():
-    import sys
-    input = sys.stdin.read().split()
-    ptr = 0
-    T = int(input[ptr])
-    ptr += 1
-    for _ in range(T):
-        N = int(input[ptr])
-        ptr += 1
-        A = list(map(int, input[ptr:ptr+N]))
-        ptr += N
-        prefix = [0]
-        current = 0
-        for num in A:
-            current += num
-            prefix.append(current)
-        Q = int(input[ptr])
-        ptr += 1
-        for __ in range(Q):
-            Q1, Q2 = map(int, input[ptr:ptr+2])
-           
+import math
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = 8 * n + 1
+    k = (math.isqrt(s) - 1) // 2
+    t_k = k * (k + 1) // 2
+    if t_k == n:
+        print(0)
+    else:
+        print(n - t_k)
