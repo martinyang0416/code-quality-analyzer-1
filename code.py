00fcc9d9eeee195621507c1c51047b8f2af0f9e2
Analyzer <analyzer@test.com>
2026-02-09 13:47:32 -0800

    Update

--- code.py
+++ code.py
@@ -1,26 +1,11 @@
-import heapq
+n = int(input())
+s = input().strip()
 
-h, w, n, r = map(int, input().split())
-
-grid = []
-start = None
-for i in range(h):
-    row = list(input().strip())
-    for j in range(w):
-        if row[j] == '@':
-            start = (i, j)
-    grid.append(row)
-
-treasures = {}
-treasure_values = []
-for i in range(n):
-    m, v = input().split()
-    v = int(v)
-    treasures[m] = (i, v)
-    treasure_values.append(v)
-
-def get_sum(mask):
-    total = 0
-    for i in range(n):
-        if mask & (1 << i):
-            total += treasure_values
+count = 0
+prev = ''
+for c in s:
+    if prev == 'x' and c == 'x':
+        break
+    count += 1
+    prev = c
+print(count)
