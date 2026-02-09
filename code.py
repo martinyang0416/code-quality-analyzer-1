4034a396db4606b719466c8aa2c11b28c6f59e97
Analyzer <analyzer@test.com>
2026-02-09 13:30:45 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,15 @@
-S = input().strip()
+X = input().strip()
+stack = []
+count = 0
 
-balance_low = 0
-balance_high = 0
-global_min = 0
-global_max = 0
+for c in X:
+    if c == 'S':
+        stack.append(c)
+    else:
+        if stack and stack[-1] == 'S':
+            stack.pop()
+            count += 1
+        else:
+            stack.append(c)
 
-for c in S:
-    if c == '0':
-        new_low = balance_low + 1
-        new_high = balance_high + 1
-        new_gmin = min(global_min, new_low)
-        new_gmax = max(global_max, new_high)
-        balance_low, balance_high = new_low, new_high
-        global_min, global_max = new_gmin, new_gmax
-    elif c == '1':
-        new_low = balance_low - 1
-        new_high = balance_high - 1
-        new_gmin = min(global_mi
+print(len(X) - 2 * count)
