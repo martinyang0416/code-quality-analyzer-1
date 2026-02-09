36b098d7bb7f31fa41d1a49fd843f38b3550b5e5
Analyzer <analyzer@test.com>
2026-02-09 10:11:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,19 @@
-t = int(input())
+import sys
+
+def can_partition(arr, target):
+    dp = [False] * (target + 1)
+    dp[0] = True
+    for num in arr:
+        for i in range(target, num - 1, -1):
+            if dp[i - num]:
+                dp[i] = True
+    return dp[target]
+
+t = int(sys.stdin.readline())
 for _ in range(t):
-    m = int(input())
-    a = list(map(int, input().split()))
-    seen = set()
-    p = []
-    for num in a:
-        if num not in seen:
-            p.append(num)
-            seen.add(num)
-    print(' '.join(map(str, p)))
+    h, v = map(int, sys.stdin.readline().split())
+    h_moves = list(map(int, sys.stdin.readline().split()))
+    v_moves = list(map(int, sys.stdin.readline().split()))
+    
+    sum_h = sum(h_moves)
+    sum_v = sum
