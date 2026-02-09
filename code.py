db90249591c22f049a33ddf8047649713cc4a50d
Analyzer <analyzer@test.com>
2026-02-09 14:54:59 -0800

    Update

--- code.py
+++ code.py
@@ -1,2 +1,9 @@
-def nth_person_gets_nth_seat(n):
-    return 1.0 if n == 1 else 0.5
+def findMin(nums):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+    return nums[left]
