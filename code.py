73b0433aafc837db3b1b5728d1598d72fb80855d
Analyzer <analyzer@test.com>
2026-02-09 10:10:56 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,11 @@
-import sys
-from collections import deque
-
-def main():
-    input = sys.stdin.read
-    data = input().split()
-    ptr = 0
-    t = int(data[ptr])
-    ptr += 1
-    for _ in range(t):
-        n = int(data[ptr])
-        k = int(data[ptr+1])
-        ptr += 2
-        friends = list(map(int, data[ptr:ptr+k]))
-        ptr += k
-        adj = [[] for _ in range(n + 1)]
-        for __ in range(n - 1):
-            u = int(data[ptr])
-            v = int(data[ptr+1])
-            adj[u].append(v)
-            adj
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    a = list(map(int, input().split()))
+    seen = set()
+    p = []
+    for num in a:
+        if num not in seen:
+            p.append(num)
+            seen.add(num)
+    print(' '.join(map(str, p)))
