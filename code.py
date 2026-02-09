cb6d998e5623ec7ac9dab0a1162369ef3bbae46d
Analyzer <analyzer@test.com>
2026-02-09 10:11:55 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,19 @@
-import sys
+from collections import defaultdict
 
-def can_partition(arr, target):
-    dp = [False] * (target + 1)
-    dp[0] = True
-    for num in arr:
-        for i in range(target, num - 1, -1):
-            if dp[i - num]:
-                dp[i] = True
-    return dp[target]
+def count_balanced_substrings(s):
+    balance = 0
+    prefix_counts = defaultdict(int)
+    prefix_counts[0] = 1  # To handle the empty prefix
+    result = 0
 
-t = int(sys.stdin.readline())
-for _ in range(t):
-    h, v = map(int, sys.stdin.readline().split())
-    h_moves = list(map(int, sys.stdin.readline().split()))
-    v_moves = list(map(int, sys.stdin.readline().split()))
-    
-    sum_h = sum(h_moves)
-    sum_v = sum
+    for char in s:
+        if char == 'a':
+            balance += 1
+        else:
+            balance -= 1
+
+        # Add the number of times this balance was seen before to the result
+        result += prefix_counts.get(balance, 0)
+
+        # Update the count for the current balance
+        prefix_count
