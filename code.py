20a096a7b59e14fb309414625d5e2d02f3a70179
Analyzer <analyzer@test.com>
2026-02-09 14:13:59 -0800

    Update

--- code.py
+++ code.py
@@ -1,17 +1,9 @@
-class Solution:
-    def calculate(self, s: str) -> int:
-        stack = []
-        result = 0
-        current_num = 0
-        sign = 1  # 1 for positive, -1 for negative
-
-        for c in s:
-            if c == ' ':
-                continue
-            if c.isdigit():
-                current_num = current_num * 10 + int(c)
-            else:
-                # Add the current number to the result with the current sign
-                result += sign * current_num
-                current_num = 0
-   
+def validateStackSequences(pushed, popped):
+    stack = []
+    j = 0
+    for num in pushed:
+        stack.append(num)
+        while stack and j < len(popped) and stack[-1] == popped[j]:
+            stack.pop()
+            j += 1
+    return j == len(popped)
