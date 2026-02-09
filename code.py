a9ab90e92c7cfbeb2fbc46f6be8cb692bd8078c0
Analyzer <analyzer@test.com>
2026-02-09 14:46:10 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,9 @@
-def minMalwareSpread(graph, initial):
-    n = len(graph)
-    initial_sorted = sorted(initial)
-    min_m = float('inf')
-    result = -1
-    
-    for x in initial_sorted:
-        # Create modified graph by removing all edges connected to x
-        modified = [row.copy() for row in graph]
-        for i in range(n):
-            modified[x][i] = 0
-            modified[i][x] = 0
-        modified[x][x] = 1  # Restore self-loop
-        
-        # New initial list without x
-        new_initial = [node fo
+def hIndex(citations):
+    citations.sort()
+    n = len(citations)
+    max_h = 0
+    for i in range(n):
+        current_h = min(citations[i], n - i)
+        if current_h > max_h:
+            max_h = current_h
+    return max_h
