adb535b7b2b3777c2ec3d3cb6d44a37a53439d96
Analyzer <analyzer@test.com>
2026-02-09 13:59:48 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,5 @@
-import sys
-from sys import stdin
-sys.setrecursionlimit(1 << 25)
-
-def find_centroid(n, edges):
-    parent = [0]*(n+1)
-    size = [1]*(n+1)
-    def dfs(u, p):
-        size[u] = 1
-        for v in edges[u]:
-            if v != p:
-                dfs(v, u)
-                size[u] += size[v]
-        parent[u] = p
-    dfs(1, -1)
-    for u in range(1, n+1):
-        max_sub = 0
-        for v in edges[u]:
-            if v != parent[u]:
-                max_sub = max(max_sub, size[v])
-        max_sub = max
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    k = (n - 1) // 2
+    print(4 * k * (k + 1) * (2 * k + 1) // 3)
