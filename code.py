64afd5b54e53b15c5f8c7c79ef24a2241f6945be
Analyzer <analyzer@test.com>
2026-02-09 13:45:05 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,22 @@
-from collections import deque, defaultdict
+import sys
+from math import gcd
 
-def min_jumps(start, end, color_map, colors):
-    if start == end:
-        return 0
-    N = len(colors)
-    visited = [False] * N
-    processed_colors = set()
-    q = deque([(start, 0)])
-    visited[start] = True
-    
-    while q:
-        pos, dist = q.popleft()
-        if pos == end:
-            return dist
-        # Move left
-        if pos > 0 and not visited[pos - 1]:
-            visited[pos - 1] = True
-            q.append((pos - 1, dist + 1))
-   
+def main():
+    max_a = 10**6
+    spf = list(range(max_a + 1))
+    for i in range(2, int(max_a**0.5) + 1):
+        if spf[i] == i:
+            for j in range(i * i, max_a + 1, i):
+                if spf[j] == j:
+                    spf[j] = i
+
+    input_data = sys.stdin.read().split()
+    n = int(input_data[0])
+    a = list(map(int, input_data[1:n+1]))
+
+    prime_counts = {}
+    for num in a:
+        if num == 1:
+            continue
+        x = num
+        seen_
