f5dd7d48b1a9aa3eed638b8883c4aa3fcb67c9e7
Analyzer <analyzer@test.com>
2026-02-09 14:52:50 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,18 @@
-from collections import Counter
-
-def getHint(secret: str, guess: str) -> str:
-    bulls = 0
-    s_non_bull = []
-    g_non_bull = []
-    for s, g in zip(secret, guess):
-        if s == g:
-            bulls += 1
-        else:
-            s_non_bull.append(s)
-            g_non_bull.append(g)
-    s_counter = Counter(s_non_bull)
-    g_counter = Counter(g_non_bull)
-    cows = sum((s_counter & g_counter).values())
-    return f"{bulls}A{cows}B"
+def longestSubarray(nums):
+    sum_ones = sum(nums)
+    if sum_ones == 0:
+        return 0
+    max_len = 0
+    left = 0
+    zero_count = 0
+    for right in range(len(nums)):
+        if nums[right] == 0:
+            zero_count += 1
+        while zero_count > 1:
+            if nums[left] == 0:
+                zero_count -= 1
+            left += 1
+        current_len = right - left + 1
+        if current_len > max_len:
+            max_len = current_len
+    return max_len - 1 if max_len > 0 else 0
