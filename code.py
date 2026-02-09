11240d4604ffe49743b95782b8397e7fc5661145
Analyzer <analyzer@test.com>
2026-02-09 11:24:33 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,19 @@
-import bisect
+import sys
 
-def putaway(A, B, T, X, Y, W, S):
-    # Categorize the toys into W_only, S_only, Both
-    W_only = []
-    S_only = []
-    Both = []
-    for w, s in zip(W, S):
-        can_weak = any(w < x for x in X) if A > 0 else False
-        can_small = any(s < y for y in Y) if B > 0 else False
-        if can_weak and can_small:
-            Both.append((w, s))
-        elif can_weak:
-            W_only.append(w)
-        elif can_small:
-            S_only.append(s)
-        else:
-            retur
+def main():
+    A, B, T = map(int, sys.stdin.readline().split())
+    X = list(map(int, sys.stdin.readline().split())) if A > 0 else []
+    Y = list(map(int, sys.stdin.readline().split())) if B > 0 else []
+    W = []
+    S = []
+    for _ in range(T):
+        wi, si = map(int, sys.stdin.readline().split())
+        W.append(wi)
+        S.append(si)
+    
+    # Compute X_max and Y_max
+    X_max = max(X) if A > 0 else 0
+    Y_max = max(Y) if B > 0 else 0
+    
+    problematic = 0
+    S_imp 
