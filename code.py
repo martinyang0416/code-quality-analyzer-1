009b8fa77964cf0f688169bd6247aaa69ef3dcfe
Analyzer <analyzer@test.com>
2026-02-09 14:03:15 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,24 @@
-k, d, t = map(int, input().split())
+import sys
 
-m = (k + d - 1) // d
-cycle_len = m * d
-on_time = k
-off_time = cycle_len - on_time
-progress_per_cycle = 2 * on_time + off_time
-total_progress_needed = 2 * t
-
-full_cycles = total_progress_needed // progress_per_cycle
-remainder = total_progress_needed % progress_per_cycle
-
-if remainder == 0:
-    total_time = full_cycles * cycle_len
-else:
-    if remainder <= 2 * on_time:
-        time_add = remainder / 2.0
-    else:
-        time_add = on_time + (remainder - 2 * on
+def main():
+    max_n = 10**6
+    rsum = [0] * (max_n + 1)
+    rsum[0] = 0
+    for n in range(1, max_n + 1):
+        if n == 1:
+            rsum[n] = 1
+        elif n == 2:
+            rsum[n] = 2
+        elif n == 3:
+            rsum[n] = 6
+        elif n == 4:
+            rsum[n] = 6
+        elif n == 5:
+            rsum[n] = 3
+        else:
+            rsum[n] = 9
+    
+    pre_sum = [0] * (max_n + 1)
+    for i in range(1, max_n + 1):
+        pre_sum[i] = pre_sum[i-1] + rsum[i]
+   
