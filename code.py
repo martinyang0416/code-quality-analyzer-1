e4491ed5e6beaf6855d0806dc207f75a645c3830
Analyzer <analyzer@test.com>
2026-02-09 14:22:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,11 @@
-def find_min_fibonacci_numbers(k):
-    fibs = [1, 1]
-    while True:
-        next_fib = fibs[-1] + fibs[-2]
-        if next_fib > k:
-            break
-        fibs.append(next_fib)
-    count = 0
-    for num in reversed(fibs):
-        if k >= num:
-            count += 1
-            k -= num
-            if k == 0:
-                break
-    return count
+def findMaxForm(strs, m, n):
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+    for s in strs:
+        zeros = s.count('0')
+        ones = len(s) - zeros
+        if zeros > m or ones > n:
+            continue
+        for i in range(m, zeros - 1, -1):
+            for j in range(n, ones - 1, -1):
+                dp[i][j] = max(dp[i][j], dp[i - zeros][j - ones] + 1)
+    return dp[m][n]
