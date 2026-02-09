08b8879badcc51f2cd53b917670ed726ad15ea01
Analyzer <analyzer@test.com>
2026-02-09 15:12:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,18 @@
-n = int(input())
-girls = list(map(int, input().split()))
-boys = []
-for s in girls:
-    if s == 2:
-        boys.append(s ^ 3)
-    else:
-        boys.append(s ^ 2)
-print(' '.join(map(str, boys)))
+n, x = map(int, input().split())
+A = list(map(int, input().split()))
+original_sum = sum(A)
+delta = [(a / x) - a for a in A]
+
+if n == 0:
+    min_sub = 0
+else:
+    current_min = delta[0]
+    min_sub = delta[0]
+    for i in range(1, n):
+        current_min = min(delta[i], current_min + delta[i])
+        if current_min < min_sub:
+            min_sub = current_min
+
+sum_delta = min(min_sub, 0.0)
+result = original_sum + sum_delta
+print("{0:.10f}".format(result))
