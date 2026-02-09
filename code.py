b5941c81fc67aaa31873e940856a6a29419b0479
Analyzer <analyzer@test.com>
2026-02-09 10:15:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,20 @@
-from collections import deque
+n = int(input())
+employees = []
+sumT = 0
+for _ in range(n):
+    t, r = map(int, input().split())
+    employees.append((r, t))
+    sumT += t
 
-def longest_temperature_span():
-    import sys
-    n = int(sys.stdin.readline())
-    temps = list(map(int, sys.stdin.readline().split()))
-    if n == 0:
-        print(0)
-        return
+employees.sort(reverse=True, key=lambda x: x[0])
 
-    max_deque = deque()
-    min_deque = deque()
-    left = 0
-    max_len = 0
+current_sum = 0
+max_val = 0
+for r, t in employees:
+    current_sum += t
+    temp = current_sum + r
+    if temp > max_val:
+        max_val = temp
 
-    for right in range(n):
-        current_temp = temps[right]
-
-        # Update max_deque
-        while max_deque and temps[max_deque[-1]] <= current_temp:
-            max_deque.pop()
-        max_deque.ap
+answer = max(max_val, sumT)
+print(answer)
