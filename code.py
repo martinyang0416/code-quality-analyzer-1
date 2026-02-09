5c2f92311092fdaeb1a9737b4afa3d050e5b9573
Analyzer <analyzer@test.com>
2026-02-09 11:20:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,6 +1,8 @@
 s = input().strip()
-digits = s[1:]  # Extract the 6 digits
-two_digit = int(digits[:2])
-if '0' in digits:
-    two_digit -= 1
-print(two_digit)
+digits = list(map(int, s[1:7]))
+sum_digits = sum(digits)
+has_zero = 0 in digits
+if has_zero:
+    print(sum_digits + 10)
+else:
+    print(sum_digits + 1)
