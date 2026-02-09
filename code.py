0d2b12ab3045385537da63c2bee63532476e6c79
Analyzer <analyzer@test.com>
2026-02-09 10:09:54 -0800

    Update

--- code.py
+++ code.py
@@ -1,16 +1,22 @@
 n = int(input())
-s = input().strip()
+b = list(map(int, input().split()))
+sum_all = sum(b)
+active = [True] * (n + 1)  # active[i] indicates if token i is still active
+total_gain = 0
 
-count_ab = 0
-count_ba = 0
+for x in range(n, 0, -1):
+    current_sum = 0
+    m = x
+    while m <= n:
+        if active[m]:
+            current_sum += -b[m-1]  # b is 0-based
+        m += x
+    if current_sum > 0:
+        total_gain += current_sum
+        m = x
+        while m <= n:
+            if active[m]:
+                active[m] = False
+            m += x
 
-for i in range(n - 1):
-    current = s[i]
-    next_char = s[i + 1]
-    if current != next_char:
-        if current == 'A':
-            count_ab += 1
-        else:
-            count_ba += 1
-
-print("YES" if count_ab > count_ba else "NO")
+pr
