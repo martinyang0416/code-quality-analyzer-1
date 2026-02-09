870d5c8016355023e1ab6ebbb22c52b6b88350ab
Analyzer <analyzer@test.com>
2026-02-09 10:42:27 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,22 @@
+import sys
 from functools import lru_cache
+
+sys.setrecursionlimit(1000000)
+
+@lru_cache(maxsize=None)
+def max_currency(n):
+    if n == 0:
+        return 0
+    split1 = n // 2
+    split2 = n // 3
+    option_split = max_currency(split1) + max_currency(split2)
+    return max(n, option_split)
 
 def main():
     import sys
-    n = int(sys.stdin.readline())
-    lst = list(map(int, sys.stdin.readline().split()))
-    
-    used_numbers = set()
-    for num in lst:
-        if num != 0:
-            used_numbers.add(num)
-    
-    pre_parity = []
-    for num in lst:
-        if num == 0:
-            pre_parity.append(None)
-        else:
-            pre_parity.append(num % 2)
-    
-    missing_numbers = [x for x in range(1, n+1) if x not in used_numbers]
-    even_cou
+    for line in sys.stdin:
+        n = int(line.strip())
+        print(max_currency(n))
+
+if __name__ == '__main__':
+    main()
