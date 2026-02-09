ca9497ac14f3e15db574850752cfd48d07f08ded
Analyzer <analyzer@test.com>
2026-02-09 11:29:41 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,20 @@
-import sys
-from collections import deque
+def putaway(A, B, T, X, Y, W, S):
+    can_weak_list = [False] * T
+    can_small_list = [False] * T
 
-class Edge:
-    def __init__(self, to, rev, capacity):
-        self.to = to
-        self.rev = rev
-        self.capacity = capacity
-
-class Dinic:
-    def __init__(self, n):
-        self.size = n
-        self.graph = [[] for _ in range(n)]
-    
-    def add_edge(self, fr, to, cap):
-        forward = Edge(to, len(self.graph[to]), cap)
-        backward = Edge(fr, len(self.graph[fr]), 0)
-        self.graph[fr].append(forward)
-        self.graph[to].append(bac
+    for i in range(T):
+        w = W[i]
+        s = S[i]
+        can_weak = False
+        if A > 0:
+            for x in X:
+                if x > w:
+                    can_weak = True
+                    break
+        can_small = False
+        if B > 0:
+            for y in Y:
+                if y > s:
+                    can_small = True
+                    break
+        can_weak_list[i] = can_
