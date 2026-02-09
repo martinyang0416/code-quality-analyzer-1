8333d5b89e5f4424b13cac5855fcdf4b58ff4bda
Analyzer <analyzer@test.com>
2026-02-09 14:21:58 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,15 @@
-from collections import defaultdict
-
-def countTriplets(A):
-    pair_counts = defaultdict(int)
-    n = len(A)
-    for i in range(n):
-        for j in range(n):
-            val = A[i] & A[j]
-            pair_counts[val] += 1
-    
-    zero_counts = defaultdict(int)
-    for a in A:
-        mask = (~a) & 0xFFFF  # Ensure 16-bit mask
-        x = mask
-        while True:
-            zero_counts[x] += 1
-            if x == 0:
+def find_min_fibonacci_numbers(k):
+    fibs = [1, 1]
+    while True:
+        next_fib = fibs[-1] + fibs[-2]
+        if next_fib > k:
+            break
+        fibs.append(next_fib)
+    count = 0
+    for num in reversed(fibs):
+        if k >= num:
+            count += 1
+            k -= num
+            if k == 0:
                 break
-            x = (x - 1) & mask
-    
-    total = 0
-    fo
+    return count
