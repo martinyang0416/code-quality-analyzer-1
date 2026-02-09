7cf5b81fb72a4ff4c56964f31b6be13a1f36d609
Analyzer <analyzer@test.com>
2026-02-09 13:59:11 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,21 @@
-n, L, a = map(int, input().split())
-if n == 0:
-    print(L // a)
-else:
-    breaks = 0
-    prev_end = 0
-    for _ in range(n):
-        t, l = map(int, input().split())
-        gap = t - prev_end
-        breaks += gap // a
-        prev_end = t + l
-    # Check the time after the last customer
-    gap = L - prev_end
-    breaks += gap // a
-    print(breaks)
+import sys
+from sys import stdin
+sys.setrecursionlimit(1 << 25)
+
+def find_centroid(n, edges):
+    parent = [0]*(n+1)
+    size = [1]*(n+1)
+    def dfs(u, p):
+        size[u] = 1
+        for v in edges[u]:
+            if v != p:
+                dfs(v, u)
+                size[u] += size[v]
+        parent[u] = p
+    dfs(1, -1)
+    for u in range(1, n+1):
+        max_sub = 0
+        for v in edges[u]:
+            if v != parent[u]:
+                max_sub = max(max_sub, size[v])
+        max_sub = max
