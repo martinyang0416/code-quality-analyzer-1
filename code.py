bc4d0f2f7231747eaf5d1c65bbd542b43ac6c5aa
Analyzer <analyzer@test.com>
2026-02-09 13:32:20 -0800

    Update

--- code.py
+++ code.py
@@ -1,15 +1,6 @@
-import sys
-
-n = int(sys.stdin.readline())
-points = [tuple(map(int, sys.stdin.readline().split())) for _ in range(n)]
-
-for i in range(n):
-    a = points[i]
-    b = points[(i + 1) % n]
-    c = points[(i + 2) % n]
-    cross = (b[0] - a[0]) * (c[1] - b[1]) - (b[1] - a[1]) * (c[0] - b[0])
-    if cross < 0:
-        print(0)
-        sys.exit()
-
-print(1)
+T = int(input())
+for _ in range(T):
+    n = int(input())
+    c4n = (4 * n * (4 * n - 1) * (4 * n - 2)) // 6
+    cn = (n * (n - 1) * (n - 2)) // 6
+    print(c4n - 4 * cn)
