603af9bcbe203bb4ca4a4e9f6c5984206b7a2ede
Analyzer <analyzer@test.com>
2026-02-09 10:18:18 -0800

    Update

--- code.py
+++ code.py
@@ -1,10 +1,15 @@
 n = int(input())
-a = list(map(int, input().split()))
+MOD = 10**9 + 7
 
-if n < 3:
-    print(0.0)
-else:
-    a.sort()
-    trimmed = a[1:-1]
-    mean = sum(trimmed) / len(trimmed)
-    print("{0:.1f}".format(mean))
+max_fact = 2 * n
+fact = [1] * (max_fact + 1)
+for i in range(1, max_fact + 1):
+    fact[i] = fact[i-1] * i % MOD
+
+term = fact[2 * n]
+term = term * pow(fact[n], MOD - 2, MOD) % MOD
+term = term * pow(fact[n], MOD - 2, MOD) % MOD
+
+term = term * pow(n + 1, MOD - 2, MOD) % MOD
+
+print(term)
