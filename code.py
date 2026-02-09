9daddf950e30ce5a3826d0de31508cd92c20d30a
Analyzer <analyzer@test.com>
2026-02-09 14:54:17 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,2 @@
-def maxTurbulenceSize(A):
-    if len(A) == 1:
-        return 1
-    max_length = 1
-    up = 1
-    down = 1
-    for i in range(1, len(A)):
-        if A[i] > A[i-1]:
-            new_up = down + 1
-            new_down = 1
-        elif A[i] < A[i-1]:
-            new_down = up + 1
-            new_up = 1
-        else:
-            new_up = 1
-            new_down = 1
-        up, down = new_up, new_down
-        current_max = max(up, down)
-        if current_max > max_length:
-            max_length = curre
+def nth_person_gets_nth_seat(n):
+    return 1.0 if n == 1 else 0.5
