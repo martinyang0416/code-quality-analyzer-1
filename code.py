9252b981fc4296ec36519ae90b4d1e403c7e18e1
Analyzer <analyzer@test.com>
2026-02-09 14:00:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,16 @@
-t = int(input())
-for _ in range(t):
-    n = int(input())
-    k = (n - 1) // 2
-    print(4 * k * (k + 1) * (2 * k + 1) // 3)
+n, k = map(int, input().split())
+teams = [tuple(map(int, input().split())) for _ in range(n)]
+sorted_teams = sorted(teams, key=lambda x: (-x[0], x[1]))
+
+groups = []
+current_group = []
+for team in sorted_teams:
+    if not current_group:
+        current_group.append(team)
+    else:
+        last_p, last_t = current_group[0]
+        if team[0] == last_p and team[1] == last_t:
+            current_group.append(team)
+        else:
+            groups.append(current_group)
+            current_group = [te
