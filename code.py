15c41eeb1b50f29fa938efeedd3d626ceb0bb8c2
Analyzer <analyzer@test.com>
2026-02-09 14:20:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,11 +1,12 @@
-def hasAllCodes(s: str, k: int) -> bool:
-    if len(s) < k:
-        return False
-    required = 1 << k  # 2^k
-    seen = set()
-    for i in range(len(s) - k + 1):
-        substr = s[i:i+k]
-        seen.add(substr)
-        if len(seen) == required:
-            return True
-    return len(seen) == required
+def findLength(A, B):
+    lenA, lenB = len(A), len(B)
+    dp = [0] * (lenB + 1)
+    max_len = 0
+    for i in range(lenA):
+        for j in reversed(range(lenB)):
+            if A[i] == B[j]:
+                dp[j+1] = dp[j] + 1
+                max_len = max(max_len, dp[j+1])
+            else:
+                dp[j+1] = 0
+    return max_len
