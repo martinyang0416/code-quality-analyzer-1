59202ce3b06c433f1627626e2bc309d90866a6cd
Analyzer <analyzer@test.com>
2026-02-09 13:35:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,21 @@
-import math
+import sys
+from collections import deque
 
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-a.sort()
-
-total = 0
-for i in range(n-1):
-    diff = abs(a[i+1] - a[i])
-    multiplier = math.ceil((i+1) / k)
-    total += diff * multiplier
-
-print(total)
+def main():
+    n, m = map(int, sys.stdin.readline().split())
+    adj = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        u, v = map(int, sys.stdin.readline().split())
+        adj[u].append(v)
+        adj[v].append(u)
+    
+    color = [-1] * (n + 1)
+    possible = True
+    
+    for u in range(1, n + 1):
+        if color[u] == -1:
+            if not adj[u]:
+                color[u] = 0
+                continue
+            queue = deque([u])
+      
