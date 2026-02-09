2df9c0a5d7983fb5209530a5d1c7731bc39d9397
Analyzer <analyzer@test.com>
2026-02-09 14:37:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,12 @@
-MOD = 10**9 + 7
-
-def numPermsDISequence(S: str) -> int:
-    n = len(S)
-    dp = [[0] * (n + 2) for _ in range(n + 2)]
-    dp[0][0] = 1
-    
-    for i in range(1, n + 1):
-        prefix = [0] * (i + 2)
-        for j in range(i + 1):
-            prefix[j + 1] = (prefix[j] + dp[i - 1][j]) % MOD
-        
-        for j in range(i + 1):
-            if S[i - 1] == 'D':
-                dp[i][j] = (prefix[i] - prefix[j]) % MOD
-            else:
-                dp[i][j] = prefix[j] % MOD
-    
-    return s
+def minPatches(nums, n):
+    patches = 0
+    miss = 1
+    i = 0
+    while miss <= n:
+        if i < len(nums) and nums[i] <= miss:
+            miss += nums[i]
+            i += 1
+        else:
+            miss += miss
+            patches += 1
+    return patches
