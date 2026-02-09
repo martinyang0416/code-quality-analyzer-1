15bb420176a89e8c25addb6f7b55fdb631e0f2f5
Analyzer <analyzer@test.com>
2026-02-09 13:29:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,10 @@
-ax, ay, bx, by, cx, cy = map(int, input().split())
-
-# Coefficients for perpendicular bisectors of AB and BC
-A1 = 2 * (bx - ax)
-B1 = 2 * (by - ay)
-C1 = (ax**2 + ay**2) - (bx**2 + by**2)
-
-A2 = 2 * (cx - bx)
-B2 = 2 * (cy - by)
-C2 = (bx**2 + by**2) - (cx**2 + cy**2)
-
-# Calculate determinant for line intersection
-D = A1 * B2 - A2 * B1
-
-if D != 0:
-    # Calculate intersection point O
-    x = (B1 * C2 - B2 * C1) / D
-    y = (A2 * C1 - A1 * C2) / D
-
-    # Vectors from O to A, B, C
-    oax = ax - x
-    o
+n, r = map(int, input().split())
+size = 1 << n
+c = list(map(int, input().split()))
+total = sum(c)
+print("{0:.6f}".format(total / size))
+for _ in range(r):
+    z, g = map(int, input().split())
+    total += g - c[z]
+    c[z] = g
+    print("{0:.6f}".format(total / size))
