1189276d05c3009b440e9900b83fcc2a46597ff3
Analyzer <analyzer@test.com>
2026-02-09 10:19:49 -0800

    Update

--- code.py
+++ code.py
@@ -1,5 +1,18 @@
-m = int(input())
-h = list(map(int, input().split()))
-h.sort()
-result = [h[i-1] for i in range(1, len(h))]
-print(' '.join(map(str, result)))
+t = int(input())
+for _ in range(t):
+    m = int(input())
+    b = list(map(int, input().split()))
+    b.sort(reverse=True)
+    mia = 0
+    friend = 0
+    for i in range(m):
+        if i % 2 == 0:
+            mia += b[i]
+        else:
+            friend += b[i]
+    if mia > friend:
+        print("WIN")
+    elif mia < friend:
+        print("LOSE")
+    else:
+        print("DRAW")
