0f7e16842cca1a4882816fc7920d24854735656d
Analyzer <analyzer@test.com>
2026-02-09 14:39:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,12 @@
-def countSubstrings(s):
-    def expand(l, r):
-        count = 0
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            count += 1
-            l -= 1
-            r += 1
-        return count
-    
-    res = 0
-    for i in range(len(s)):
-        res += expand(i, i)    # odd length palindromes
-        res += expand(i, i+1)  # even length palindromes
-    return res
+def canCompleteCircuit(gas, cost):
+    start = 0
+    total = 0
+    current = 0
+    for i in range(len(gas)):
+        diff = gas[i] - cost[i]
+        total += diff
+        current += diff
+        if current < 0:
+            start = i + 1
+            current = 0
+    return start if total >= 0 else -1
