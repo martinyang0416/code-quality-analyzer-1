e79a5c3584b95e7d5eebb47a0dc58081c2101159
Analyzer <analyzer@test.com>
2026-02-09 15:03:02 -0800

    Update

--- code.py
+++ code.py
@@ -1,29 +1,7 @@
-def myAtoi(s: str) -> int:
-    INT_MIN = -2**31
-    INT_MAX = 2**31 - 1
-
-    i = 0
-    n = len(s)
-    
-    # Skip leading whitespaces
-    while i < n and s[i] == ' ':
-        i += 1
-    
-    if i >= n:
-        return 0
-    
-    # Determine sign
-    sign = 1
-    if s[i] in '+-':
-        sign = -1 if s[i] == '-' else 1
-        i += 1
-    
-    # Collect digits
-    digits = []
-    while i < n and s[i].isdigit():
-        digits.append(s[i])
-        i += 1
-    
-    if not digits:
-        return 0
-    
+n, *rest = map(int, open(0).read().split())
+a = rest[:n]
+total_xor = 0
+for num in a:
+    total_xor ^= num
+result = [x ^ total_xor for x in a]
+print(' '.join(map(str, result)))
