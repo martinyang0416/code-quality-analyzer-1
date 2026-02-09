cd85b62282ac6f6ae017796b5483c1e52eeaad08
Analyzer <analyzer@test.com>
2026-02-09 15:15:53 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,24 @@
-import sys
-from sys import stdin
+import bisect
+from collections import defaultdict
 
-def main():
-    input = sys.stdin.read().split()
-    ptr = 0
-    t = int(input[ptr])
-    ptr += 1
-    for _ in range(t):
-        n, m, k = map(int, input[ptr:ptr+3])
-        ptr +=3
-        classes = list(map(int, input[ptr:ptr+n]))
-        ptr +=n
-        present = [False]*(k+1)
-        for c in classes:
-            if 1 <= c <=k:
-                present[c] = True
-        all_present = True
-        for i in range(1, k+1):
-            if not present[i]:
-        
+# Read input
+data = list(map(int, input().split()))
+n = data[0]
+T = data[1]
+S = data[2:2+n]
+
+# Precompute sum_map for all (k, l) pairs where k < l
+sum_map = defaultdict(list)
+for k in range(n):
+    for l in range(k + 1, n):
+        current_sum = S[k] + S[l]
+        sum_map[current_sum].append(k)
+
+# Sort each list in sum_map for binary search
+for s in sum_map:
+    sum_map[s].sort()
+
+total = 0
+
+# Iterate over all (i, j) pairs with i < j
+for j in r
