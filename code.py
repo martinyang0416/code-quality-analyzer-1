4285586dc0abf5b2f42b3e11e1e04a271607119c
Analyzer <analyzer@test.com>
2026-02-09 10:19:18 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,5 @@
-def main():
-    import sys
-    M = int(sys.stdin.readline())
-    B = list(map(int, sys.stdin.readline().split()))
-    if M < 4:
-        print(0)
-        return
-
-    INF = float('inf')
-    prev_dp = [INF] * 4  # [gap0, gap1, gap2, gap3]
-    # Initialize first element (index 0)
-    prev_dp[0] = B[0]       # selected
-    prev_dp[1] = 0          # not selected
-    
-    for i in range(1, M):
-        current_dp = [INF] * 4
-        current_val = B[i]
-        for gap_prev in range(4):
-            if pre
+m = int(input())
+h = list(map(int, input().split()))
+h.sort()
+result = [h[i-1] for i in range(1, len(h))]
+print(' '.join(map(str, result)))
