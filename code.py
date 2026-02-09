066ec279ac460d5882327dcd3b07807b3907c21a
Analyzer <analyzer@test.com>
2026-02-09 14:53:37 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,20 @@
-def longestSubarray(nums):
-    sum_ones = sum(nums)
-    if sum_ones == 0:
-        return 0
-    max_len = 0
-    left = 0
-    zero_count = 0
-    for right in range(len(nums)):
-        if nums[right] == 0:
-            zero_count += 1
-        while zero_count > 1:
-            if nums[left] == 0:
-                zero_count -= 1
-            left += 1
-        current_len = right - left + 1
-        if current_len > max_len:
-            max_len = current_len
-    return max_len - 1 if max_len > 0 else 0
+def maxTurbulenceSize(A):
+    if len(A) == 1:
+        return 1
+    max_length = 1
+    up = 1
+    down = 1
+    for i in range(1, len(A)):
+        if A[i] > A[i-1]:
+            new_up = down + 1
+            new_down = 1
+        elif A[i] < A[i-1]:
+            new_down = up + 1
+            new_up = 1
+        else:
+            new_up = 1
+            new_down = 1
+        up, down = new_up, new_down
+        current_max = max(up, down)
+        if current_max > max_length:
+            max_length = curre
