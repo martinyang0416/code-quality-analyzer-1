b17699d39990b67642737d55aa2891329ad66104
Analyzer <analyzer@test.com>
2026-02-09 10:24:33 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,21 @@
+import sys
+
+MOD = 10**9 + 7
+
 def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    T = int(data[idx])
-    idx += 1
-    for _ in range(T):
-        N = int(data[idx])
-        idx += 1
-        temps = list(map(int, data[idx:idx+N]))
-        idx += N
-        if N <= 1:
-            print(0)
-        else:
-            print(max(temps) - min(temps))
+    # Read input
+    N, K, Q = map(int, sys.stdin.readline().split())
+    input_line = sys.stdin.readline().split()
+    p = int(input_line[0])
+    q = int(input_line[1])
+    r = int(input_line[2])
+    s = int(input_line[3])
+    u = int(input_line[4])
+    v = int(input_line[5])
+    w = int(input_line[6])
+    x = int(input_line[7])
+    y = int(input_line[8])
+    G1 = int(input_line[9])
     
-if __name__ == "__main__":
-    main()
+    L1_line = sys.stdin.readline().split()
+    L1 = int(L1_line[0])
