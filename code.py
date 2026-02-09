2063faf801ca747c4f9d1d34879cea79b2820a72
Analyzer <analyzer@test.com>
2026-02-09 14:27:36 -0800

    Update

--- code.py
+++ code.py
@@ -1,14 +1,18 @@
-from collections import defaultdict
-from typing import List
+import collections
 
-class Solution:
-    def numSubarraysWithSum(self, A: List[int], S: int) -> int:
-        count = defaultdict(int)
-        count[0] = 1
-        prefix_sum = 0
-        res = 0
-        for num in A:
-            prefix_sum += num
-            res += count.get(prefix_sum - S, 0)
-            count[prefix_sum] += 1
-        return res
+def longestPrefix(nums):
+    current_freq = collections.defaultdict(int)
+    freq_freq = collections.defaultdict(int)
+    max_length = 0
+
+    for i, num in enumerate(nums):
+        old_count = current_freq[num]
+        if old_count > 0:
+            freq_freq[old_count] -= 1
+            if freq_freq[old_count] == 0:
+                del freq_freq[old_count]
+        current_freq[num] += 1
+        new_count = current_freq[num]
+        freq_freq[new_count] += 1
+
+        if len(fre
