d7f82e660e5c3133d26ac287e3a28545eef39344
Analyzer <analyzer@test.com>
2026-02-09 10:40:42 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,15 @@
-def main():
-    import sys
-    input = sys.stdin.read
-    data = input().split()
-    idx = 0
-    q = int(data[idx])
-    idx += 1
-    for _ in range(q):
-        m, p = int(data[idx]), int(data[idx+1])
-        idx +=2
-        b = list(map(int, data[idx:idx+m]))
-        idx +=m
-        
-        # Compute good array
-        n = m-1
-        good = [0] * n
-        for i in range(n):
-            if b[i] > 3 * b[i+1]:
-                good[i] = 1
-        
-        # Compute prefix sum
-        prefix = [0]
+import bisect
+
+n = int(input())
+arr = list(map(int, input().split()))
+rev_arr = arr[::-1]
+
+tails = []
+for num in rev_arr:
+    idx = bisect.bisect_left(tails, num)
+    if idx == len(tails):
+        tails.append(num)
+    else:
+        tails[idx] = num
+
+print(len(tails))
