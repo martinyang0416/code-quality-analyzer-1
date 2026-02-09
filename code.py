d35dead5041d809ff9851b47ad55c27d69bf0103
Analyzer <analyzer@test.com>
2026-02-09 14:14:38 -0800

    Update

--- code.py
+++ code.py
@@ -1,9 +1,20 @@
-def validateStackSequences(pushed, popped):
-    stack = []
-    j = 0
-    for num in pushed:
-        stack.append(num)
-        while stack and j < len(popped) and stack[-1] == popped[j]:
-            stack.pop()
-            j += 1
-    return j == len(popped)
+class TrieNode:
+    def __init__(self):
+        self.children = [None, None]
+
+def findMaximumXOR(nums):
+    if len(nums) < 2:
+        return 0
+    max_xor = 0
+    root = TrieNode()
+    # Insert first number into trie
+    num = nums[0]
+    current = root
+    for i in range(30, -1, -1):
+        bit = (num >> i) & 1
+        if not current.children[bit]:
+            current.children[bit] = TrieNode()
+        current = current.children[bit]
+    # Process remaining numbers
+    for num in nums[1:]:
+   
