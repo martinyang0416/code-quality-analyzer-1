dd934c73261c2cd25ad68e6dac6b28aa28d5ff04
Analyzer <analyzer@test.com>
2026-02-09 13:53:04 -0800

    Update

--- code.py
+++ code.py
@@ -1,18 +1,26 @@
-cnt1, cnt2, x, y = map(int, input().split())
-lcm = x * y  # Since x and y are distinct primes
+import sys
+from bisect import bisect_right
+from collections import defaultdict
 
-low = 1
-high = 10**18
+n = int(sys.stdin.readline())
+balls = list(map(int, sys.stdin.readline().split()))
+balls.sort()
 
-while low < high:
-    mid = (low + high) // 2
-    a = mid - mid // x
-    b = mid - mid // y
-    c = mid - mid // lcm
-    
-    if a >= cnt1 and b >= cnt2 and c >= (cnt1 + cnt2):
-        high = mid
-    else:
-        low = mid + 1
+H = defaultdict(int)
 
-print(low)
+for i in range(n):
+    a = balls[i]
+    for j in range(i):
+        j_val = balls[j]
+        d = a - j_val
+        H[d] += 1
+
+F = defaultdict(int)
+for d1 in H:
+    for d2 in H:
+        s = d1 + d2
+        F[s] += H[d1] * H[d2]
+
+sorted_ds = sorted(H.keys())
+prefix = [0] * (len(sorted_ds) + 1)
+for i in 
