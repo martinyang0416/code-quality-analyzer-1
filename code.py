61caa58bc35ee5aa887deb48f0723f4e421e6faa
Analyzer <analyzer@test.com>
2026-02-09 11:32:21 -0800

    Update

--- code.py
+++ code.py
@@ -1,20 +1,23 @@
 import sys
-from collections import deque
+sys.setrecursionlimit(1 << 25)
 
 def main():
-    sys.setrecursionlimit(1 << 25)
     N = int(sys.stdin.readline())
     s = sys.stdin.readline().strip()
-    edges = [[] for _ in range(N + 1)]
-    for _ in range(N - 1):
+    cowfolk = [i+1 for i in range(N) if s[i] == '1']
+    M = len(cowfolk)
+    if M == 0:
+        for _ in range(N):
+            print(0)
+        return
+
+    edges = [[] for _ in range(N+1)]
+    for _ in range(N-1):
         a, b = map(int, sys.stdin.readline().split())
         edges[a].append(b)
         edges[b].append(a)
 
-    S = []
-    for i in range(1, N + 1):
-        if s[i - 1] == '1':
-            S.append(i)
-    M = len(S)
-    if M == 0:
-        return  # problem states there is 
+    root = cowfolk[0]
+
+    parent = [0]*(N+1)
+    
