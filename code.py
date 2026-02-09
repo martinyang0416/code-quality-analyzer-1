499505455bb96c2cdb69bb2b9cf955aed2146928
Analyzer <analyzer@test.com>
2026-02-09 13:28:14 -0800

    Update

--- code.py
+++ code.py
@@ -1,27 +1,18 @@
-from collections import Counter
-
-a = input().strip()
-b = input().strip()
-c = input().strip()
-
-count_a = Counter(a)
-count_b = Counter(b)
-count_c = Counter(c)
-
-# Compute x_max
-x_max = float('inf')
-for ch in count_b:
-    if count_b[ch] == 0:
-        continue
-    available = count_a.get(ch, 0)
-    max_x = available // count_b[ch]
-    if max_x < x_max:
-        x_max = max_x
-if x_max == float('inf'):
-    x_max = 0
-
-max_total = -1
-best_x = 0
-best_y = 0
-
-for x in range(x_max, -1, -1):  # Iterate from x_
+class DSU:
+    def __init__(self, size):
+        self.parent = list(range(size))
+        self.rank = [1] * size
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        x_root = self.find(x)
+        y_root = self.find(y)
+        if x_root == y_root:
+            return
+        if self.rank[x_root] < self.rank[y_root]:
+            self.parent[x_root] = y_root
+        else
