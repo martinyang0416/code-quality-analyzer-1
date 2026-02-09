aa79f5fc41c16413ec9cc835f24734a97ea3a024
Analyzer <analyzer@test.com>
2026-02-09 11:16:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,10 @@
 a1, a2, a3 = map(int, input().split())
-n = a3 + 1
-
-if n == 1:
-    print(a1)
-elif n == 2:
-    print(a2)
+term_num = a3 + 1
+a, b = a1, a2
+if term_num == 2:
+    print(b)
 else:
-    sequence = [a1, a2]
-    for i in range(2, n):
-        next_term = sequence[i-2] + sequence[i-1]
-        sequence.append(next_term)
-    print(sequence[-1])
+    for _ in range(3, term_num + 1):
+        next_term = a + b
+        a, b = b, next_term
+    print(b)
