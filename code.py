ddcaf101aa710ea1556408607b75007a76f4d254
Analyzer <analyzer@test.com>
2026-02-09 10:28:16 -0800

    Update

--- code.py
+++ code.py
@@ -1,22 +1,10 @@
 def main():
     import sys
-    input = sys.stdin.read
-    data = input().split()
-    
-    index = 0
-    T = int(data[index])
-    index += 1
-    for _ in range(T):
-        n = int(data[index])
-        m = int(data[index + 1])
-        index += 2
-        
-        grid = []
-        for i in range(n):
-            line = data[index]
-            index += 1
-            grid.append([int(c) for c in line])
-        
-        if grid[0][0] == 1 or grid[n-1][m-1] == 1:
-            print(0)
-            continu
+    n = int(sys.stdin.readline())
+    alerts = list(map(int, sys.stdin.readline().split()))
+    max_alert = max(alerts)
+    # The rest of the input (edges) can be ignored as the structure doesn't affect the result
+    print(max_alert)
+
+if __name__ == "__main__":
+    main()
