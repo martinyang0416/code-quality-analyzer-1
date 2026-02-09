b68fb46bf7996eb00a547b8d0d97cb09aa7de7e2
Analyzer <analyzer@test.com>
2026-02-09 10:14:26 -0800

    Update

--- code.py
+++ code.py
@@ -1,27 +1,22 @@
-n_players = int(input())
-A = 0
-P = 0
+s = input().strip()
 
-for _ in range(n_players):
-    name, cat = input().split()
-    if cat == 'amateur':
-        A += 1
+args = []
+current_arg = []
+state = 'outside'
+
+for char in s:
+    if state == 'outside':
+        if char == ' ':
+            if current_arg:
+                args.append(''.join(current_arg))
+                current_arg = []
+        elif char == '[':
+            if current_arg:
+                args.append(''.join(current_arg))
+                current_arg = []
+            state = 'inside'
+        else:
+            current_arg.append(char)
     else:
-        P += 1
-
-max_ap = max(A, P)
-min_ap = min(A, P)
-
-n = 1
-while True:
-    if n % 2 == 0:
-        half = (n * n) // 2
-        if max_ap <= half:
-            print(n)
-            break
-    else:
-        large_half = (n * n + 1) // 2
-        small_half = (n * n - 1) // 2
-        if max_ap <= large_half and min_ap <= small_half:
-            print(n)
-  
+        if char == ']':
+     
