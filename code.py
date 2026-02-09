5a524565a4370d9376ea390847a8c2f7610bd63e
Analyzer <analyzer@test.com>
2026-02-09 13:23:39 -0800

    Update

--- code.py
+++ code.py
@@ -1,21 +1,24 @@
-def subtract_one(s):
-    s_list = list(s)
-    i = len(s_list) - 1
-    while i >= 0 and s_list[i] == '0':
-        s_list[i] = '9'
-        i -= 1
-    if i < 0:
-        return '0'
-    s_list[i] = str(int(s_list[i]) - 1)
-    new_s = ''.join(s_list).lstrip('0')
-    return new_s if new_s else '0'
+n, m, a, b, p, q = map(int, input().split())
 
-def compute(s, m):
-    if m == 0:
-        return 0
-    X = list(map(int, s))
-    n = len(X)
-    from functools import lru_cache
-
-    @lru_cache(maxsize=None)
-    def dp(pos, mod, prev_digit, last_dir, tight,
+if a == 1 and b == 1:
+    print(n)
+else:
+    low = 0
+    high = n - 1
+    k_low = -1
+    while low <= high:
+        mid = (low + high) // 2
+        a_pow = a ** mid
+        b_pow = b ** mid
+        current = p * a_pow + q * b_pow
+        if current <= m:
+            k_low = mid
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    candidates = []
+    if k_low == -1:
+        candidates.append(0)
+    else:
+        candidates.appen
