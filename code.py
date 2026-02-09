9e23b0c51d9c3c7571474472b3aaa6b43e20ad73
Analyzer <analyzer@test.com>
2026-02-09 14:24:08 -0800

    Update

--- code.py
+++ code.py
@@ -1,13 +1,5 @@
-def containsNearbyAlmostDuplicate(nums, k, t):
-    if k < 0 or t < 0:
-        return False
-    bucket_map = {}
-    index_map = {}
-    for i in range(len(nums)):
-        # Remove the element out of window
-        remove_index = i - k - 1
-        if remove_index >= 0:
-            if remove_index in index_map:
-                old_bucket = index_map[remove_index]
-                if old_bucket in bucket_map and bucket_map[old_bucket][1] == remove_index:
-                    del bucket_map[old_bucket]
+import math
+from functools import reduce
+
+def is_good_array(nums):
+    return reduce(math.gcd, nums) == 1
