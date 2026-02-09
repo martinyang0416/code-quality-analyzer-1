af3ef20d68e4eb0594835fc91fc8e5f54911473a
Analyzer <analyzer@test.com>
2026-02-09 15:17:07 -0800

    Update

--- code.py
+++ code.py
@@ -1,19 +1,13 @@
-import sys
-from collections import deque
+def minimal_length(s):
+    stack = []
+    for char in s:
+        if stack and stack[-1] == char:
+            stack.pop()
+        else:
+            stack.append(char)
+    return len(stack)
 
-def main():
-    sys.setrecursionlimit(1 << 25)
-    N = int(sys.stdin.readline())
-    traffic = [0] * (N + 1)
-    for i in range(1, N + 1):
-        traffic[i] = int(sys.stdin.readline())
-    adj = [[] for _ in range(N + 1)]
-    for _ in range(N - 1):
-        u, v = map(int, sys.stdin.readline().split())
-        adj[u].append(v)
-        adj[v].append(u)
-    
-    # Build parent pointers using BFS
-    parent = [0] * (N + 1)
-    visited = [False] * (N + 1)
-  
+T = int(input())
+for _ in range(T):
+    s = input().strip()
+    print(minimal_length(s))
